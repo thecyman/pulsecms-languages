5538d5b27bbe37e8e34293e29ad6f066bb18c3cd
yuzoolcode <michaelfrankland@Michaels-MacBook-Pro.local>
2016-01-16 14:26:03 +0900

    Finnish added!

---
+++ finnish.php
@@ -0,0 +1,96 @@
+<?php
+
+//Global
+
+$lang_cancel = "Peruuta";
+$lang_help = "Help";
+$lang_save = "Tallenna";
+$lang_rename_btn = "Nimeä uudelleen";
+$lang_title = "Pulse CMS";
+
+// Login
+
+$lang_login_incorrect = "Virheellinen salasana!";
+$lang_login_password = "Salasana";
+$lang_login_button = "Kirjaudu sisään";
+$lang_nav_logout = "Kirjaudu ulos";
+
+// Home
+$lang_home_emptyfold = "Tämä kansio on tyhjä.";
+$lang_home_new = "Luo uusi";
+$lang_home_upload_button = "Lataa tiedosto";
+$lang_home_preview = "Esikatsele";
+
+// Navigation
+
+$lang_nav_home = "Etusivu";
+$lang_nav_blocks = "Osiot";
+$lang_nav_img = "Media";
+$lang_nav_blog = "Blogi";
+$lang_nav_pages = "Sivut";
+$lang_nav_stats = "Tilastot";
+
+// Gallery
+
+$lang_gal_caption_gallery = "Kirjoita kuvateksti";
+$lang_gal_filename = "Tiedostonimi";
+$lang_gal_dimensions = "Mitat";
+$lang_gal_size = "Koko";
+$lang_gal_img = "IMG";
+$lang_gal_link = "Link";
+$lang_gallery_tool_tip = "Luodaksesi lightbox-gallerian, luo kuvakansio ja käytä embed-tagia.";
+
+// Pages
+
+$lang_pages_title = "Sivun otsikko";
+$lang_pages_description = "Sivun kuvaus";
+
+// Create
+
+$lang_create_file_or_folder = "Luo uusi tiedosto tai kansio";
+$lang_create_button = "Luo";
+$lang_create_tool_tip = 'Sivuilla, osioilla ja blogiartikkeleilla tulee olla ".txt" -tiedostopääte. Kansion luodaksesi jätä tiedostopääte pois.';
+
+// Delete
+
+$lang_delete = "Haluatko varmasti poistaa ";
+$lang_del_button = "Poista";
+
+// Errors
+
+$lang_error_create_ext = "Virheellinen tiedostopääte.";
+$lang_error_file_exists = "Kansiosta löytyy jo tiedosto tällä nimellä.";
+$lang_error_upload = "Sinun tulee olla Galleriassa ladataksesi kuvan.";
+$lang_no_content = "sisältöä ei löydetty";
+
+//Blog
+
+$lang_blog_read_more = "Lue lisää";
+$lang_blog_older = "Vanhemmat";
+$lang_blog_newer = "Uudemmat";
+$lang_blog_error_folder = "Et voi luoda kansiota Blogiin";
+$lang_blog_tool_tip = "Blogiartikkelit on numerojärjestyksessä ja suurin numero on uusin artikkeli.";
+
+//Form
+
+$lang_form_error1 = "Ole hyvä ja täytä kenttä ";
+$lang_form_error2a = "Syöttämäsi arvo kenttään ";
+$lang_form_error2b = " on liian pitkä.";
+$lang_form_subject_line = "Ota yhteyttä";
+$lang_form_email_sent = "Sähköposti lähetettiin";
+$lang_form_sent_button = "Lähetä";
+
+// Stats
+
+$lang_stats_today = "Vierailut";
+$lang_stats_refers = "Suosituimmat viittaavat sivut";
+$lang_stats_pages = "Suosituimmat sivut";
+$lang_stats_refresh = "Päivitä";
+$lang_stats_pageviews = "Sivulataukset";
+$lang_stats_per_visit = "Sivuja / vierailu";
+$lang_stats_online = "Online";
+$lang_stats_thisweek = "Tämä viikko";
+$lang_stats_todays_stats = "Tänään";
+$lang_stats_nodata = "Ei dataa";
+
+?>
