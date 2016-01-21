81e2ce5633bf5de1b34dad5175c72226ffa32492
yuzoolcode <michaelfrankland@Michaels-MacBook-Pro.local>
2016-01-21 20:07:20 +0900

    Danish is here!

---
+++ danish.php
@@ -0,0 +1,74 @@
+<?php
+//Global
+$lang_cancel = "Annulér";
+$lang_help = "Hjælp";
+$lang_save = "Gem";
+$lang_rename_btn = "Omdøb";
+$lang_title = "Pulse CMS";
+$lang_version = "Pulse CMS 4.5.2";
+$lang_update = "Opdatér";
+// Login
+$lang_login_incorrect = "Dit password er forkert!";
+$lang_login_password = "Password";
+$lang_login_button = "Login";
+$lang_nav_logout = "Logout";
+// Home
+$lang_home_emptyfold = "Mappen er tom."; 
+$lang_home_new = "Ny";
+$lang_home_upload_button = "Upload";
+$lang_home_preview = "Eksempel";
+// Navigation
+$lang_nav_home = "Hjem";
+$lang_nav_blocks = "Blokke";
+$lang_nav_img = "Media";
+$lang_nav_blog = "Blog";
+$lang_nav_pages = "Sider";
+$lang_nav_stats = "Statistik";
+// Gallery
+$lang_gal_caption_gallery = "Indsæt billedtekst her";
+$lang_gal_filename = "Filnavn";
+$lang_gal_dimensions = "Dimensioner";
+$lang_gal_size = "Størrelse";
+$lang_gal_img = "IMG";
+$lang_gal_link = "Link";
+$lang_gallery_tool_tip = "For at oprette et Lightbox galleri, skal du oprette en mappe med billeder og bruge et 'EMBED' tag.";
+// Pages
+$lang_pages_title = "Sidetitel";
+$lang_pages_description = "Sidebeskrivelse";
+// Create
+$lang_create_file_or_folder = 'Ny fil eller mappe';
+$lang_create_button = "Lav"; 
+$lang_create_tool_tip = "Sider, blokke og blog indlæg skal ende med '.txt'. Undlad endelsen, for at lave en mappe.";
+// Delete
+$lang_delete = 'Er du sikker på du vil slette ';
+$lang_del_button = "Slet";
+// Errors
+$lang_error_create_ext = 'IUgyldig fil endelse.';
+$lang_error_file_exists = 'En fil med det navn eksisterer allerede i denne mappe.';
+$lang_error_upload = 'Du skal stå i en galleri for at lægge et billede op .';
+$lang_no_content = 'ingen indhold fundet';
+//Blog
+$lang_blog_read_more = 'Læs mere';
+$lang_blog_older = 'Ældre';
+$lang_blog_newer = 'Nyere';
+$lang_blog_error_folder = "Du kan ikke lav en mappe i Blog";
+$lang_blog_tool_tip = "Blog indlæg er i nummereret orden, det højeste nummer er det nyeste indlæg.";
+//Form
+$lang_form_error1 = 'Udfyld venligst feltet ';
+$lang_form_error2a = 'Dit felt ';
+$lang_form_error2b = ' er for langt.';
+$lang_form_subject_line = 'Kontakt';
+$lang_form_email_sent = 'Din mail er sendt';
+$lang_form_sent_button = 'Send';
+// Stats
+$lang_stats_today = "Besøgende";
+$lang_stats_refers = "Top henvisninger";
+$lang_stats_pages = "Top sider";
+$lang_stats_refresh = "Genindlæs";
+$lang_stats_pageviews = "Sidevisninger";
+$lang_stats_per_visit = "Sider / Besøg";
+$lang_stats_online = "Online";
+$lang_stats_thisweek = "Denne uge";
+$lang_stats_todays_stats = "I dag";
+$lang_stats_nodata = "Ingen data";
+?>
