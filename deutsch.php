8cf26d25df496cfef297fb9f378025168f2a09e1
yuzoolcode <michaelfrankland@Michaels-MacBook-Pro.local>
2016-02-15 17:42:45 +0900

    German lang improvements

--- deutsch.php
+++ deutsch.php
@@ -13,7 +13,7 @@ $lang_update = "Aktualisieren";
 // Login
 
 $lang_login_incorrect = "Falsches Passwort";
-$lang_login_password = "Passwort";
+$lang_login_password = "Password";
 $lang_login_button = "Weiter";
 $lang_nav_logout = "Abmelden";
 
@@ -27,21 +27,21 @@ $lang_home_preview = "Vorschau";
 // Navigation
 
 $lang_nav_home = "Home";
-$lang_nav_blocks = "Blocks";
+$lang_nav_blocks = "Bl&ouml;cke";
 $lang_nav_img = "Medien";
 $lang_nav_blog = "Blog";
 $lang_nav_pages = "Seiten";
-$lang_nav_stats = "Statistiken";
+$lang_nav_stats = "Statisitik";
 
 // Gallery
 
 $lang_gal_caption_gallery = "Bildbeschreibung hier eingeben.";
 $lang_gal_filename = "Dateiname";
 $lang_gal_dimensions = "Dimensionen";
-$lang_gal_size = "Gr&ouml;&szlig;e";
+$lang_gal_size = "Gr&ouml;sse";
 $lang_gal_img = "IMG";
 $lang_gal_link = "Link";
-$lang_gallery_tool_tip = "Um eine Lightbox Galerie zu erstellen, erstellen Sie hier einen neuen Ordner mit Bildern und nutzen Sie den gegebenen Einbettungs-Code.";
+$lang_gallery_tool_tip = "Um eine Lightbox Galerie zu erstellen, m&uuml;ssen Sie hier einen neuen Ordner mit Bildern erstellen. Zum einf&uuml;gen der Galerie nutzen Sie die Embed Tags.";
 
 
 // Pages
@@ -53,17 +53,17 @@ $lang_pages_description = "Seitenbeschreibung";
 
 $lang_create_file_or_folder = 'Neue Datei oder Ordner';
 $lang_create_button = "Erstellen";
-$lang_create_tool_tip = " Neue Seiten, Bl&ouml;cke und Blogposts m&uuml;ssen mit einem '.txt' enden. Um einen Ordner zu erstellen, einfach keine Erweiterung hinzuf&uuml;gen. ";
+$lang_create_tool_tip = " Neue Seiten, Bl&ouml;cke und Blogposts m&uuml;ssen mit einem '.txt' enden. Um ein Ordner zu erstellen einfach keine Erweiterung hinz&uuml;fugen. ";
 
 // Delete
 
-$lang_delete = 'M&ouml;chten Sie diese Datei wirklich l&ouml;schen?: ';
+$lang_delete = 'Sind Sie sicher dass Sie diese Datei l&ouml;schen m&ouml;chten: ';
 $lang_del_button = "L&ouml;schen";
 
 // Errors
 
 $lang_error_create_ext = 'Ung&uuml;ltige Dateierweiterung.';
-$lang_error_file_exists = 'Eine Datei mit diesen Namen existiert bereits in diesem Ordner.';
+$lang_error_file_exists = 'Eine Datei mit diesem Namen in diesem Ordner existiert bereits.';
 $lang_error_upload = 'Sie m&uuml;ssen in einer Galerie sein um ein Bild hochzuladen.';
 $lang_no_content = 'Kein Inhalt gefunden.';
 
@@ -73,7 +73,7 @@ $lang_blog_read_more = 'Weiterlesen';
 $lang_blog_older = '&Auml;ltere Beitr&auml;ge';
 $lang_blog_newer = 'Neuere Beitr&auml;ge';
 $lang_blog_error_folder = "Sie k&ouml;nnen keine Ordner im $lang_nav_blog erstellen.";
-$lang_blog_tool_tip = "Blog Beitr&auml;ge sind numerisch geordnet, die h&ouml;chste Nummer ist der neueste Beitrag.";
+$lang_blog_tool_tip = "Blogbeitr&auml;ge sind numerisch geordnet, die h&ouml;chste Nummer ist der neuste Beitrag.";
 
 
 //Form
@@ -82,7 +82,7 @@ $lang_form_error1 = 'Bitte f&uuml;llen Sie das Feld aus: ';
 $lang_form_error2a = 'Das Feld ';
 $lang_form_error2b = ' ist zu lang.';
 $lang_form_subject_line = 'Kontakt';
-$lang_form_email_sent = 'Ihre Email wurde gesendet.';
+$lang_form_email_sent = 'Ihre E-Mail wurde gesendet.';
 $lang_form_sent_button = 'Abschicken';
 
 // Stats
@@ -95,7 +95,7 @@ $lang_stats_pageviews = "Besuchte Seiten";
 $lang_stats_per_visit = "Seiten / Besuch";
 $lang_stats_online = "Online";
 $lang_stats_thisweek = "Diese Woche";
-$lang_stats_todays_stats = "Heutige Statistiken";
+$lang_stats_todays_stats = "Heutige Statistik";
 $lang_stats_nodata = "Keine Daten";
 
 ?>
