b8de5de48f114330d9b8d210caf8f2e920da5adf
yuzoolcode <michaelfrankland@Michaels-MacBook-Pro.local>
2016-09-08 15:14:33 +0900

    Tagalog added!

---
+++ tagalog.php
@@ -0,0 +1,98 @@
+<?php
+//Global
+
+$lang_cancel = "Kanselahin";
+$lang_help = "Tulong";
+$lang_save = "Save";
+$lang_rename_btn = "Palitan ang Pangalan";
+$lang_title = "Pulse CMS";
+$lang_version = "Pulse CMS 4.5.2";
+$lang_update = "Update";
+
+// Login
+$lang_login_incorrect = "Mali ang iyong Password!";
+$lang_login_password = "Password";
+$lang_login_button = "Log In";
+$lang_nav_logout = "Log Out";
+
+// Home
+
+$lang_home_emptyfold = "Ang folder na ito ay walang laman."; 
+$lang_home_new = "Bago";
+$lang_home_upload_button = "Mag-upload";
+$lang_home_preview = "Preview";
+
+// Navigation
+
+$lang_nav_home = "Home";
+$lang_nav_blocks = "Blocks";
+$lang_nav_img = "Media";
+$lang_nav_blog = "Blog";
+$lang_nav_pages = "Pahina";
+$lang_nav_stats = "Stats";
+
+// Gallery
+
+$lang_gal_caption_gallery = "Ipasok caption dito";
+$lang_gal_filename = "Filename";
+$lang_gal_dimensions = "Mga Dimensyon";
+$lang_gal_size = "Sukat";
+$lang_gal_img = "IMG";
+$lang_gal_link = "Link";
+$lang_gallery_tool_tip = "Upang lumikha ng isang lightbox gallery, lumikha ng isang folder ng mga imahe at gamitin ang naka-embed na tag.";
+
+// Pages
+
+$lang_pages_title = "Pamagat ng Pahina";
+$lang_pages_description = "Paglalarawan ng Pahina";
+
+// Create
+
+$lang_create_file_or_folder = 'New file o folder';
+$lang_create_button = "Lumikha"; 
+$lang_create_tool_tip = "Mga Pahina, mga blocks at mga post sa blog ay dapat magtapos sa 'txt'. Upang lumikha ng isang folder, magdagdag ng walang extension.";
+
+// Delete
+
+$lang_delete = 'Sigurado ka bang gusto mong burahin ';
+$lang_del_button = "Alisin";
+
+// Errors
+
+$lang_error_create_ext = 'Hindi wastong file extension.';
+$lang_error_file_exists = 'Ang isang file na may ganitong pangalan sa folder na ito ay mayroon na.';
+$lang_error_upload = 'Kailangan mong na nasa Gallery ka upang mag-upload ng isang imahe.';
+$lang_no_content = 'Walang laman';
+
+//Blog
+
+$lang_blog_read_more = 'Magbasa Nang Higit pa';
+$lang_blog_older = 'Luma';
+$lang_blog_newer = 'Bago';
+$lang_blog_error_folder = "Hindi ka maaaring lumikha ng isang folder sa Blog";
+$lang_blog_tool_tip = "Ang Blog entries ay nakaayos sa pamamagitan ng mga numero, ang pinakamataas ang magiging bagong post. ";
+
+//Form
+
+$lang_form_error1 = 'Mangyaring punan ang field ';
+$lang_form_error2a = 'Ang iyong field ';
+$lang_form_error2b = ' ay masyadong mahaba.';
+$lang_form_subject_line = 'Contact';
+$lang_form_email_sent = 'Napadala na ang Email';
+$lang_form_sent_button = 'Ipadala';
+
+// Stats
+
+$lang_stats_today = "Bisita";
+$lang_stats_refers = "Top Referrers";
+$lang_stats_pages = "Top Pages";
+$lang_stats_refresh = "Refresh";
+$lang_stats_pageviews = "Pageviews";
+$lang_stats_per_visit = "Mga Pahina / Bisita";
+$lang_stats_online = "Online";
+$lang_stats_thisweek = "Ngayong Linggo";
+$lang_stats_todays_stats = "Ngayon";
+$lang_stats_nodata = "No Data";
+
+
+?>
