05fa3065341d672419d102355ab806ea63161e7d
Nomadzy <aachraf@gmail.com>
2016-10-26 17:37:58 +0100

    Create arabic.php
    arabic language file. work in progress ..

---
+++ arabic.php
@@ -0,0 +1,100 @@
+<?php
+//Global
+
+$lang_cancel = "الغاء";
+$lang_help = "مساعدة";
+$lang_save = "تسجيل";
+$lang_rename_btn = "اعادة التسمية";
+$lang_title = "Pulse CMS";
+$lang_version = "Pulse CMS 4.5.2";
+$lang_update = "تحديث";
+
+// Connexion
+
+$lang_login_incorrect = "كلمة السر غير صحيحة";
+$lang_login_password = "كلمة السر";
+$lang_login_button = "دخول";
+$lang_nav_logout = "مغادرة";
+
+// Accueil
+$lang_home_emptyfold = "ملف فارغ"; 
+$lang_home_new = "جديد";
+$lang_home_upload_button = "تحميل";
+$lang_home_preview = "لمحة";
+
+// Navigation
+
+$lang_nav_home = "الرئيسية";
+$lang_nav_blocks = "كثلة";
+$lang_nav_img = "وسائط";
+$lang_nav_blog = "مدونة";
+$lang_nav_pages = "صفحات";
+$lang_nav_stats = "احصائيات";
+
+
+// Galerie
+
+$lang_gal_caption_gallery = "هنا أدخل العنوان";
+$lang_gal_filename = "اسم الملف";
+$lang_gal_dimensions = "أبعاد";
+$lang_gal_size = "وزن";
+$lang_gal_img = "رمز الصورة";
+$lang_gal_link = "رمز الوصلة";
+$lang_gallery_tool_tip = "لإنشاء معرض لايةبوكس, أنشء مجلد الصور واستخدمعلامة تضمين.";
+
+
+// Pages
+
+$lang_pages_title = "Titre de la page";
+$lang_pages_description = "Description de la page";
+
+// Création
+
+$lang_create_file_or_folder = 'Nouveau fichier ou dossier';
+$lang_create_button = "Créer"; 
+$lang_create_tool_tip = "Les nouvelles pages, les nouveaux blocs et articles de blog doivent se terminer par '.txt'. Pour créer un dossier, n'ajoutez pas d'extension.";
+
+// Suppression
+
+$lang_delete = 'Êtes-vous sûr de vouloir supprimer ';
+$lang_del_button = "Supprimer";
+
+// Erreurs
+
+$lang_error_create_ext = 'Vous ne pouvez créer que des fichiers textes (.txt) et des dossiers';
+$lang_error_file_exists = 'Un fichier avec ce nom existe déjà dans ce dossier';
+$lang_error_upload = 'Vous devez être dans une galerie pour importer une image';
+$lang_no_content = 'pas de contenu trouvé';
+
+//Blog
+
+$lang_blog_read_more = 'Lire la suite';
+$lang_blog_older = 'Articles anciens';
+$lang_blog_newer = 'Articles récents';
+$lang_blog_error_folder = "Vous ne pouvez pas créer un dossier dans le blog";
+$lang_blog_tool_tip = "Blog entries are ordered by number, the highest numbering being the newest post.";
+
+//Formulaire
+
+$lang_form_error1 = 'Saisissez le champ : ';
+$lang_form_error2a = 'Le champ ';
+$lang_form_error2b = ' est trop long.';
+$lang_form_subject_line = 'Contact';
+$lang_form_email_sent = 'L\'email a été envoyé';
+$lang_form_sent_button = 'Envoyer';
+
+// Statistiques
+
+$lang_stats_today = "Visiteurs";
+$lang_stats_refers = "Meilleurs réferents";
+$lang_stats_pages = "Pages plus vues";
+$lang_stats_refresh = "Rafraîchir";
+$lang_stats_pageviews = "Pages vues";
+$lang_stats_per_visit = "Pages / Visite";
+$lang_stats_online = "En ligne";
+$lang_stats_thisweek = "Cette semaine";
+$lang_stats_todays_stats = "Aujourd'hui";
+$lang_stats_nodata = "Pas de données";
+
+
+?>
