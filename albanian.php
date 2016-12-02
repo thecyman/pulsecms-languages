c69ab8cb49b688964af23adc814eb60ab9cab6cc
yuzoolcode <michaelfrankland@Michaels-MacBook-Pro.local>
2016-12-02 09:42:34 +0900

    Albanian

---
+++ albanian.php
@@ -0,0 +1,100 @@
+<?php
+// Përgjithshme
+
+$lang_cancel = "Anulojë";
+$lang_help = "Ndihmë";
+$lang_save = "Ruaje";
+$lang_rename_btn = "Riemërto";
+$lang_title = "Pulse CMS";
+$lang_version = "Pulse CMS 4.6";
+$lang_update = "Përditëso";
+
+// Identifikimi
+
+$lang_login_incorrect = "Fjalëkalim i gabuar. Provo përsëri.";
+$lang_login_password = "Fjalëkalimi";
+$lang_login_button = "Identifikohu";
+$lang_nav_logout = "Dilni";
+
+// Ballina
+$lang_home_emptyfold = "Kjo dosje është bosh."; 
+$lang_home_new = "Krijo";
+$lang_home_upload_button = "Ngarko";
+$lang_home_preview = "Parapamje";
+
+// Butonat
+
+$lang_nav_home = "Ballina";
+$lang_nav_blocks = "Dosjet";
+$lang_nav_img = "Imazhet";
+$lang_nav_blog = "Blogu";
+$lang_nav_pages = "Faqet";
+$lang_nav_stats = "Statistikat";
+
+// Galeria
+
+$lang_gal_caption_gallery = "Vendos titullin këtu";
+$lang_gal_filename = "Emri i skedarit";
+$lang_gal_dimensions = "Dimensioni";
+$lang_gal_size = "Madhësia";
+$lang_gal_img = "IMG";
+$lang_gal_link = "Lidhja";
+$lang_gallery_tool_tip = "Për të krijuar një lightbox gallery, krijoni një dosje të imazheve dhe të përdorni embed tag.";
+
+// Faqet
+
+$lang_pages_title = "Titulli i faqës";
+$lang_pages_description = "Përshkrimi i faqës";
+
+// Krijo
+
+$lang_create_file_or_folder = 'Krijo skedar ose dosje';
+$lang_create_button = "Krijo"; 
+$lang_create_tool_tip = "Faqet, dosje dhe blog postimet duhet të përfundojnë me '.txt'. Për të krijuar një dosje, vetëm të shkruani emrin.";
+
+// Fshi
+
+$lang_delete = 'Jeni te sigurte ju doni të fshini ';
+$lang_del_button = "Fshije";
+
+// Gabimet
+
+$lang_error_create_ext = 'Skedari i pavlefshëm shtesë';
+$lang_error_file_exists = 'Një skedë me këtë emër në këtë dosje tashmë ekziston.';
+$lang_error_upload = 'Ju duhet të jetë në një Gallery për të ngarkoni një ose më shumë fotografi.';
+$lang_no_content = 'asnjë përmbajtje nuk u gjet';
+
+// Blogu
+
+$lang_blog_read_more = 'Lexo më shumë';
+$lang_blog_older = 'Më të vjetra';
+$lang_blog_newer = 'Më të reja';
+$lang_blog_error_folder = "Ju nuk mund të krijoni një dosje në Blog";
+$lang_blog_tool_tip = "Blog postimet janë të renditura nga numri, me i lartë duke qenë pas më i ri.";
+$lang_blog_import = 'Importo';
+$lang_blog_import_tool_tip = 'Importo blog postimet nga RSS.
+Importuesi do të importojë nga RSS dhe ATOM formatet.';
+$lang_blog_import_url = 'URL';
+
+// Forma
+
+$lang_form_error1 = 'Ju lutemi plotësoni këtë fushë ';
+$lang_form_error2a = 'fushën tuaj ';
+$lang_form_error2b = ' është shumë e gjatë.';
+$lang_form_subject_line = 'Kontakt';
+$lang_form_email_sent = 'Email është dërguar';
+$lang_form_sent_button = 'Dërgo';
+
+// Statistikat
+
+$lang_stats_today = "Vizitorë";
+$lang_stats_refers = "Top Referimet";
+$lang_stats_pages = "Top Faqet";
+$lang_stats_refresh = "Rifresko";
+$lang_stats_pageviews = "Faqe të shikuara";
+$lang_stats_per_visit = "Faqe / Vizita";
+$lang_stats_online = "Në Linjë";
+$lang_stats_thisweek = "Këtë Javë";
+$lang_stats_todays_stats = "Sot";
+$lang_stats_nodata = "Nuk ka të dhëna";
+?>
