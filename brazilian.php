542224a22bdfcfff0a63f22b99d119f1564ac5fe
Lucas Tsolakian <LucasTsolakian@users.noreply.github.com>
2016-12-06 08:39:45 +0200

    Update brazilian.php

--- brazilian.php
+++ brazilian.php
@@ -18,9 +18,9 @@ $lang_nav_logout = "Sair";
 
 // Home
 
-$lang_home_emptyfold = "Esta pasta está vazia."; 
+$lang_home_emptyfold = "Esta pasta está vazia"; 
 $lang_home_new = "Novo";
-$lang_home_upload_button = "Subir";
+$lang_home_upload_button = "Envio";
 $lang_home_preview = "Visualização";
 
 // Navigation
@@ -45,7 +45,7 @@ $lang_gallery_tool_tip = "Para criar uma galeria de fotos, crie uma pasta de ima
 
 // Pages
 
-$lang_pages_meta = "Meta";
+$lang_pages_meta = "Meta Tags";
 $lang_pages_title = "Titlo da Página";
 $lang_pages_description = "Descrição da Página";
 
@@ -54,32 +54,32 @@ $lang_pages_description = "Descrição da Página";
 $lang_create_file_or_folder = 'Novo arquivo ou pasta';
 $lang_create_button = "Criar"; 
 $lang_create_tool_tip = "Páginas, blocos e postagens do Blog devem ter a extenção '.txt', para criar uma pasta não adicione extenção.";
-$lang_create_embed_tag = "Embed Tag";
+$lang_create_embed_tag = "Tag Embutido";
 
 // Delete
 
-$lang_delete = 'Tem certeza que quer apagar ';
+$lang_delete = 'Tem certeza que quer apagar?';
 $lang_del_button = "Apagar";
 
 // Errors
 
 $lang_error_create_ext = 'Extenção de arquivo invalida.';
 $lang_error_file_exists = 'Nessa pasta ja existe um arquivo com esse nome.';
-$lang_error_upload = 'Você tem que estar em uma Galeria para subir uma imagem.';
-$lang_no_content = 'conteúdo não encontrado';
+$lang_error_upload = 'Você tem que estar em uma Galeria para enviar uma imagem.';
+$lang_no_content = 'Conteúdo não encontrado';
 
 //Blog
 
-$lang_blog_title = 'Blog Title';
-$lang_blog_date = 'Date';
+$lang_blog_title = 'Titlo do Blog';
+$lang_blog_date = 'Data';
 $lang_blog_read_more = 'Ler mais';
 $lang_blog_older = 'Anteriores';
 $lang_blog_newer = 'Recentes';
 $lang_blog_error_folder = "Você não pode criar uma pasta no Blog.";
 $lang_blog_tool_tip = "Postagens do Blog são numeradas, números mais altos sendo os mais recentes.";
-$lang_blog_import = 'Import';
-$lang_blog_import_tool_tip = 'Import blog entries from RSS.
-The importer will import from RSS and ATOM formats.';
+$lang_blog_import = 'Importar';
+$lang_blog_import_tool_tip = 'Importar postagens do blog pela RSS.
+O sistema vai importar em formatos RSS e ATOM.';
 $lang_blog_import_url = 'URL';
 
 
@@ -100,7 +100,7 @@ $lang_stats_pages = "Páginas Principais";
 $lang_stats_refresh = "Refrescar";
 $lang_stats_pageviews = "Visualizações";
 $lang_stats_per_visit = "Páginas / Visita";
-$lang_stats_bounce_rate ="Bounce Rate";
+$lang_stats_bounce_rate ="Taxa de rejeição";
 $lang_stats_online = "Conectado";
 $lang_stats_thisweek = "Essa Semana";
 $lang_stats_todays_stats = "Hoje";
