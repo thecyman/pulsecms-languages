4360b8f3ed57787bf55c334142e587b2b18d918b
oliver-77 <oliver.kalkbrenner@internetfx.de>
2016-12-22 09:31:19 +0100

    Update deutsch.php

--- deutsch.php
+++ deutsch.php
@@ -18,7 +18,7 @@ $lang_nav_logout = "Abmelden";
 
 // Home
 
-$lang_home_emptyfold = "Dieser Ordner ist leer."; 
+$lang_home_emptyfold = "Dieser Ordner ist leer.";
 $lang_home_new = "Neu";
 $lang_home_upload_button = "Hochladen";
 $lang_home_preview = "Vorschau";
@@ -26,11 +26,11 @@ $lang_home_preview = "Vorschau";
 // Navigation
 
 $lang_nav_home = "Home";
-$lang_nav_blocks = "Blocks";
-$lang_nav_img = "Media";
+$lang_nav_blocks = "Bl&ouml;cke";
+$lang_nav_img = "Medien";
 $lang_nav_blog = "Blog";
-$lang_nav_pages = "Pages";
-$lang_nav_stats = "Stats";
+$lang_nav_pages = "Seiten";
+$lang_nav_stats = "Statistik";
 
 // Gallery
 
@@ -40,7 +40,7 @@ $lang_gal_dimensions = "Dimensionen";
 $lang_gal_size = "Gr&ouml;sse";
 $lang_gal_img = "IMG";
 $lang_gal_link = "Link";
-$lang_gallery_tool_tip = "Um eine Lightbox Gallerie zu erstellen, erstellen Sie hier einen neuen Ordner mit Bildern und nutzen Sie den gebenen Embed code.";
+$lang_gallery_tool_tip = "Um eine Lightbox Galerie zu erstellen, m&uuml;ssen Sie hier einen neuen Ordner mit Bildern erstellen. Zum einf&uuml;gen der Galerie nutzen Sie die Embed Tags.";
 
 
 // Pages
@@ -53,19 +53,19 @@ $lang_pages_description = "Seitenbeschreibung";
 
 $lang_create_file_or_folder = 'Neue Datei oder Ordner';
 $lang_create_button = "Erstellen";
-$lang_create_tool_tip = " Neue Seiten, Bl&ouml;cke und Blogposts m&uuml;ssen mit einem '.txt' enden. Um ein Ordner zu erstellen einfach keine Extention hinz&uuml;fugen. ";
+$lang_create_tool_tip = " Neue Seiten, Bl&ouml;cke und Blogposts m&uuml;ssen mit einem '.txt' enden. Um ein Ordner zu erstellen einfach keine Erweiterung hinz&uuml;fugen. ";
 $lang_create_embed_tag = "Tag zum Einfügen";
 
 // Delete
 
-$lang_delete = 'Sind Sie sicher Sie m&ouml;chten diese Datei l&ouml;schen: ';
+$lang_delete = 'Sind Sie sicher dass Sie diese Datei l&ouml;schen m&ouml;chten: ';
 $lang_del_button = "L&ouml;schen";
 
 // Errors
 
 $lang_error_create_ext = 'Ung&uuml;ltige Dateierweiterung.';
-$lang_error_file_exists = 'Eine Datei mit diesen Namen in diesen Ordner existiert bereits.';
-$lang_error_upload = 'Sie m&uuml;ssen in einer Gallerie sein um ein Bild hochzuladen.';
+$lang_error_file_exists = 'Eine Datei mit diesem Namen in diesem Ordner existiert bereits.';
+$lang_error_upload = 'Sie m&uuml;ssen in einer Galerie sein um ein Bild hochzuladen.';
 $lang_no_content = 'Kein Inhalt gefunden.';
 
 //Blog
@@ -78,9 +78,9 @@ $lang_blog_newer = 'Neuere Beitr&auml;ge';
 $lang_blog_error_folder = "Sie k&ouml;nnen keine Ordner im $lang_nav_blog erstellen.";
 $lang_blog_tool_tip = "Blog Beitr&auml;ge sind numerisch geordnet, die h&ouml;hste Nummer ist der neuste Beitrag.";
 $lang_blog_import = 'Importieren';
-$lang_blog_import_tool_tip = 'Importiere Blog Einträge aus RSS. Der Importer importiert RSS und Atom Format.';
-$lang_blog_import_page_max = 'Maximale Anzahl von Seiten';
-$lang_blog_import_page_var = 'URL Variable Name für Seiten';
+$lang_blog_import_tool_tip = 'Importiere Blog Einträge aus RSS. Der Importer importiert RSS und Atom Formate.';
+$lang_blog_import_page_max = 'Maximale Seitenanzahl';
+$lang_blog_import_page_var = 'Variable fuer Paginierung';
 $lang_blog_import_url = 'URL';
 
 
@@ -90,7 +90,7 @@ $lang_form_error1 = 'Bitte f&uuml;llen Sie das Feld aus: ';
 $lang_form_error2a = 'Das Feld ';
 $lang_form_error2b = ' ist zu lang.';
 $lang_form_subject_line = 'Kontakt';
-$lang_form_email_sent = 'Ihre Email wurde gesendet.';
+$lang_form_email_sent = 'Ihre E-Mail wurde gesendet.';
 $lang_form_sent_button = 'Abschicken';
 
 // Stats
@@ -101,10 +101,10 @@ $lang_stats_pages = "Beliebteste Seiten:";
 $lang_stats_refresh = "Aktualisieren";
 $lang_stats_pageviews = "Besuchte Seiten";
 $lang_stats_per_visit = "Seiten / Besuch";
-$lang_stats_bounce_rate ="Abweisungsrate";
+$lang_stats_bounce_rate ="Absprungrate";
 $lang_stats_online = "Online";
 $lang_stats_thisweek = "Diese Woche";
-$lang_stats_todays_stats = "Heutige Stats";
+$lang_stats_todays_stats = "Heutige Statistik";
 $lang_stats_nodata = "Keine Daten";
 
 ?>
