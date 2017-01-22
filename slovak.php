d5786e51de58f76079c15ab33a6a2b40a0209357
yuzoolcode <michaelfrankland@Michaels-MacBook-Pro.local>
2017-01-22 21:04:28 +0900

    Slovak updated

--- slovak.php
+++ slovak.php
@@ -67,23 +67,23 @@ $lang_no_content = 'nenašiel žiadny obsah';
 
 //Blog
 
-$lang_blog_title = 'Blog Title';
-$lang_blog_date = 'Date';
+$lang_blog_title = 'Názov Blogu';
+$lang_blog_date = 'Dátum';
 $lang_blog_read_more = 'Čítajte ďalej';
 $lang_blog_older = 'Staršie';
 $lang_blog_newer = 'Novšie';
 $lang_blog_error_folder = "Nedá sa vytvoriť priečinok v Blogu";
 $lang_blog_tool_tip = "Príspevky blogu sú zoradené podľa číslovania, najvyššie číslovanie predstavuje najnovší príspevok.";
-$lang_blog_import = 'Import';
-$lang_blog_import_tool_tip = 'Import blog entries from RSS.
-The importer will import from RSS and ATOM formats.';
-$lang_blog_import_page_max = 'Max number of pages';
-$lang_blog_import_page_var = 'Paging variable name';
+$lang_blog_import = 'Importovať';
+$lang_blog_import_tool_tip = 'Import položiek blogu z RSS.
+Import sa bude preberať z RSS a ATOM formátov.';
+$lang_blog_import_page_max = 'Maximálny počet stránok';
+$lang_blog_import_page_var = 'Názov premennej stránkovania';
 $lang_blog_import_url = 'URL';
 
 //Form
 
-$lang_form_error1 = 'Prisim vyplnte pole ';
+$lang_form_error1 = 'Prosim vyplnte pole ';
 $lang_form_error2a = 'Vaše údaje ';
 $lang_form_error2b = ' je príliš dlhé.';
 $lang_form_subject_line = 'Kontakt';
@@ -99,8 +99,8 @@ $lang_stats_pages = "Top stránky";
 $lang_stats_refresh = "Aktualizovať";
 $lang_stats_pageviews = "Zobrazenie stránok";
 $lang_stats_per_visit = "Stránky / Návštevnosť";
-$lang_stats_bounce_rate ="Bounce Rate";
-$lang_stats_online = "Online";
+$lang_stats_bounce_rate ="Miera hodnotenia";
+$lang_stats_online = "Priamy";
 $lang_stats_thisweek = "Tento týždeň";
 $lang_stats_todays_stats = "Dnes";
 $lang_stats_nodata = "Žiadne dáta";
