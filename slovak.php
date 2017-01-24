f46ccc08d2480b033c8defd05ca58a91d8f4fdef
yuzoolcode <michaelfrankland@Michaels-MacBook-Pro.local>
2017-01-24 22:19:19 +0900

    Slovak 4.7 ready

--- slovak.php
+++ slovak.php
@@ -89,7 +89,7 @@ $lang_form_error2b = ' je príliš dlhé.';
 $lang_form_subject_line = 'Kontakt';
 $lang_form_email_sent = 'E-mail bol odoslaný';
 $lang_form_sent_button = 'Odoslať';
-$lang_auto_thank_contact = 'Thank you for sending me an email - we will reply soon and have received your email';
+$lang_auto_thank_contact = 'Ďakujem za zaslanie e-mailu! Na Váš e-mail odpoviem hneď ako to bude možné.';
 
 // Stats
 
