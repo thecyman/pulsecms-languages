82c8d876176521b70fdcebd4d07d58d2b5da9a48
yuzoolcode <michaelfrankland@Michaels-MacBook-Pro.local>
2017-02-01 16:53:36 +0900

    Persian language added!

---
+++ persian.php
@@ -0,0 +1,112 @@
+<?php
+//Global
+
+$lang_cancel = "صرفه نظر";
+$lang_help = "کمک";
+$lang_save = "ذخیره";
+$lang_rename_btn = "تغییر اسم ";
+$lang_title = "پالس سی ام اس";
+$lang_update = "بروزکردن";
+
+// Login
+
+$lang_login_incorrect = "رمز عبور را وراد کنید";
+$lang_login_password = "رمز عبور";
+$lang_login_button = "ورود";
+$lang_nav_logout = "خروج";
+
+// Home
+$lang_home_emptyfold = "این پوشه خالی است.";
+$lang_home_new = "جدید";
+$lang_home_upload_button = "بارگذاری";
+$lang_home_preview = "پیش نمایش";
+
+// Navigation
+
+$lang_nav_home = "خونه";
+$lang_nav_blocks = "بلوک ها";
+$lang_nav_img = "گالری";
+$lang_nav_blog = "بلاگ";
+$lang_nav_pages = "برگه";
+$lang_nav_stats = "آمار";
+
+
+// Gallery
+
+$lang_gal_caption_gallery = "درج زیرنویس در اینجا";
+$lang_gal_filename = "اسم پوشه ";
+$lang_gal_dimensions = "ابعاد";
+$lang_gal_size = "حجم";
+$lang_gal_img = "IMG";
+$lang_gal_link = "لینک";
+$lang_gallery_tool_tip = "ایجاد یک گالری سبد،ایجاد ک پوشه از تصاویر و استفاده از تگ قراردادن.";
+
+// Pages
+
+$lang_pages_meta = "Meta";
+$lang_pages_title = "عنوان  صفحه";
+$lang_pages_description = "شرح صفحه";
+
+// Create
+
+$lang_create_file_or_folder = 'فایل یا پوشه جدید';
+$lang_create_button = "ایجاد"; 
+$lang_create_tool_tip = "صفحات، بلوک و پست های وبلاگ باید با پسوند txt ختم شود. برای ایجاد یک پوشه، اضافه کردن پسوند.";
+
+$lang_create_embed_tag = " قراردادن برچسب";
+
+
+// Delete
+
+$lang_delete = 'آیا مطمئنید که میخواهید حذف کنید';
+$lang_del_button = "حذف";
+
+// Errors
+
+$lang_error_create_ext = 'وسعت فایل نامعتبر است.';
+$lang_error_file_exists = 'یک فایل با این نام در این پوشه وجود دارد.';
+$lang_error_upload = 'شما نیاز به در یک گالری به آپلود یک تصویر.';
+$lang_no_content = 'هیچ محتوای یافت';
+
+//Blog
+
+$lang_blog_title = ' عنوان بلاگ';
+$lang_blog_date = 'تاریخ';
+$lang_blog_read_more = ' بیشتر';
+$lang_blog_older = 'قدیمی تر';
+$lang_blog_newer = 'پیام جدیدتر';
+$lang_blog_error_folder = "شما می توانید یک پوشه را در وبلاگ ایجاد کند";
+$lang_blog_tool_tip = "مطالب وبلاگ توسط تعدادی، بالاترین شماره بودن، جدید ترین پست دستور داد.";
+$lang_blog_import = 'وارد كردن';
+$lang_blog_import_tool_tip = '
+پست های ارسال شده واردات از RSS.
+وارد کننده از فرمت های RSS و ATOM وارد کنید.';
+$lang_blog_import_page_max = 'حداکثر تعداد صفحاتی';
+$lang_blog_import_page_var = 'صفحه بندی نام متغیر';
+$lang_blog_import_url = 'URL';
+
+//Form
+
+$lang_form_error1 = 'لطفا با پر کردن درست ';
+$lang_form_error2a = 'زمینه خود را';
+$lang_form_error2b = ' خیلی طولانی است.';
+$lang_form_subject_line = 'تماس';
+$lang_form_email_sent = 'ایمیل ارسال شد';
+$lang_form_sent_button = 'ارسال';
+
+// Stats
+
+$lang_stats_today = "بازدید کنندگان";
+$lang_stats_refers = "ارجاعات بالا";
+$lang_stats_pages = "صفحه بالا";
+$lang_stats_refresh = "تازه کردن";
+$lang_stats_pageviews = "تعداد بازدید صفحات";
+$lang_stats_per_visit = "صفحه / بازدید";
+$lang_stats_bounce_rate ="ارزش گذاری/ گزاف گویی";
+$lang_stats_online = "آنلاین";
+$lang_stats_thisweek = "این هفته";
+$lang_stats_todays_stats = "امروز";
+$lang_stats_nodata = "بدون تاریخ";
+
+
+?>
