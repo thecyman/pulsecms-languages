ec713870886ae23213d70fb9ce483294f031306e
yuzoolcode <michaelfrankland@Michaels-MacBook-Pro.local>
2017-04-17 10:13:24 +0900

    Italian fixed

--- italian.php
+++ italian.php
@@ -5,8 +5,8 @@ $lang_cancel = "Annulla";
 $lang_help = "Aiuto";
 $lang_save = "Salva";
 $lang_rename_btn = "Rinomina";
-$lang_title = "Pulse CMS"; // Admin pages <title>
-$lang_update = "Aggiorna"; // Admin Pulse download link
+$lang_title = "Pulse CMS";
+$lang_update = "Aggiorna";
 
 // Login
 
@@ -16,7 +16,7 @@ $lang_login_button = "Accedi";
 $lang_nav_logout = "Esci";
 
 // Home
-$lang_home_emptyfold = "Questa cartella è vuota."; 
+$lang_home_emptyfold = "Questa cartella è vuota";
 $lang_home_new = "Nuovo";
 $lang_home_upload_button = "Carica";
 $lang_home_preview = "Anteprima";
@@ -33,10 +33,10 @@ $lang_nav_stats = "Statistiche";
 
 // Gallery
 
-$lang_gal_caption_gallery = "Inserisci qui la didascalia";
+$lang_gal_caption_gallery = "Inserisci qui la tua didascalia";
 $lang_gal_filename = "Nome file";
-$lang_gal_dimensions = "Dimensioni (px)";
-$lang_gal_size = "Dimension (KB)";
+$lang_gal_dimensions = "Dimensioni in px";
+$lang_gal_size = "Dimensioni in KB";
 $lang_gal_img = "IMG";
 $lang_gal_link = "Link";
 $lang_gallery_tool_tip = "Per creare una galleria Lightbox, crea una cartella di immagini e usa il tag corrispondente.";
@@ -50,20 +50,21 @@ $lang_pages_description = "Descrizione pagina";
 // Create
 
 $lang_create_file_or_folder = 'Nuovo file o cartella';
-$lang_create_button = "Crea"; 
-$lang_create_tool_tip = "Pagine, blocchi e post devono terminare con '.txt'. Per creare una cartella non aggiungere estensioni. ";
+$lang_create_button = "Crea";
+$lang_create_tool_tip = "Pagine, blocchi e post devono terminare con .txt. Per creare una cartella non aggiungere estensioni. ";
 $lang_create_embed_tag = "Tag corrispondente";
+
 
 // Delete
 
-$lang_delete = 'Sei sicuro di voler eliminare';
+$lang_delete = 'Sei sicuro di voler eliminare ';
 $lang_del_button = "Elimina";
 
 // Errors
 
 $lang_error_create_ext = 'Estensione nome file invalida';
-$lang_error_file_exists = 'Un file con questo nome già esiste in questa cartella.';
-$lang_error_upload = "Devi essere in una galleria per caricare un'immagine.";
+$lang_error_file_exists = 'Un file con questo nome gia esiste in questa cartella.';
+$lang_error_upload = 'Devi essere in una galleria per caricare un\'immagine.';
 $lang_no_content = 'Nessun contenuto trovato';
 
 //Blog
@@ -71,13 +72,14 @@ $lang_no_content = 'Nessun contenuto trovato';
 $lang_blog_title = 'Titolo Blog';
 $lang_blog_date = 'Data';
 $lang_blog_read_more = 'Leggi tutto';
-$lang_blog_older = 'Vecchi';
-$lang_blog_newer = 'Nuovi';
+$lang_blog_back_button = 'Indietro';
+$lang_blog_older = 'Notizie precedenti';
+$lang_blog_newer = 'Notizie recenti';
 $lang_blog_error_folder = "Non puoi creare una cartella in Blog";
-$lang_blog_tool_tip = "I post del blog sono ordinati per numero, un numero maggiore indica un post più recente.";
+$lang_blog_tool_tip = "I post del blog sono ordinati per numero, un numero maggiore indica un post piu recente.";
 $lang_blog_import = 'Importa';
-$lang_blog_import_tool_tip = "Importa i post del blog tramite RSS. L'importer accetta i formati RSS e ATOM.";
-$lang_blog_import_page_max = 'Max number of pages';
+$lang_blog_import_tool_tip = 'Importa i post del blog tramite RSS. L\'importer accetta i formati RSS e ATOM.';
+$lang_blog_import_page_max = 'Numero massimo di pagine';
 $lang_blog_import_page_var = 'Paging variable name';
 $lang_blog_import_url = 'URL';
 
@@ -85,17 +87,18 @@ $lang_blog_import_url = 'URL';
 
 $lang_form_error1 = 'Per favore compila il campo: ';
 $lang_form_error2a = 'Il campo ';
-$lang_form_error2b = ' è troppo lungo.';
+$lang_form_error2b = ' e troppo lungo';
 $lang_form_subject_line = 'Contatti';
 $lang_form_email_sent = 'Email inviata ';
-$lang_form_sent_button = 'Invia ';
-$lang_auto_thank_contact = 'Grazie per l''invio dell''email - l''abbiamo ricevuta e ti risponderemo presto';
+$lang_form_sent_button = 'Invia';
+
+$lang_auto_thank_contact = 'Grazie abbiamo ricevuto il tuo messaggio, ti risponderemo presto';
 
 // Stats
 
 $lang_stats_today = "Visitatori";
-$lang_stats_refers = "Maggiori referral (traffico dai link a questo sito)";
-$lang_stats_pages = "Pagine più viste";
+$lang_stats_refers = "Maggiori referral";
+$lang_stats_pages = "Pagine piu visitate";
 $lang_stats_refresh = "Aggiorna";
 $lang_stats_pageviews = "Pagine visualizzate";
 $lang_stats_per_visit = "Pagine per Visita";
