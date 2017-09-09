9fcc8128f2ea71f54a44410d8ad95733acef84ed
IvaRo <ivarosoft@gmail.com>
2017-09-09 07:23:26 +0200

    Create czech.php

---
+++ pulse 5/complete/czech.php
@@ -0,0 +1,404 @@
+<?php
+/*
+	* PulseCMS 5 jazyky
+	* překlad: Čeština
+	* Autor: IvaRo
+	* Kontakt: ivarosoft@gmail.com
+	* Copyright (c) 2017 IvaRo Soft Slovakia	
+	* Licence: MIT
+*/
+
+//Global - Globální
+$lang_cancel = "Zrušit";
+$lang_duplicate = "Duplikát";
+$lang_help = "Nápověda";
+$lang_help_url ="http://help.pulsecms.com/";
+$lang_settings = "Nastavení";
+$lang_account = "Účet";
+$lang_save = "Uložit";
+$lang_rename_btn = "Přejmenovat";
+$lang_title = "Pulse CMS";    /* Název admin stránky */
+
+// Auto backup - Automatické zálohování
+$lang_autobackup_email_subject_line = 'Pulse automatické zálohování';
+$lang_autobackup_email_text =<<<EOD
+Dobrý den,
+
+Instalační program Pulse generoval záložní soubor zip.
+
+S pozdravem,
+Vaše Pulse CMS
+EOD;
+
+// Login - Přihlášení
+$lang_login_forgot_password = "Zapomněli jste heslo ?";
+$lang_login_forgot_password_email_body =<<<EOD
+	Dobrý den,
+	
+	Vaše heslo pro Pulse je PASSWORD
+EOD;
+$lang_login_forgot_password_email_subject_line = "Zapomněli jste heslo Pulse?"; 
+$lang_login_incorrect = "Vaše heslo je nesprávné!";
+$lang_login_password = "Heslo";
+$lang_login_button = "Přihlášení";
+$lang_nav_logout = "Odhlášení";
+$lang_login_otp = "OTP Heslo";   /* OTP = jednorázové heslo - vygenerovane */
+$lang_login_welcome = "Ahoj!";
+$lang_login_name = "Uživatel Pulse";
+
+// Home - Domovská stránka
+$lang_home_emptyfold = "Tato složka je prázdná."; 
+$lang_home_new = "Nový";
+$lang_home_upload_button = "Nahrát";
+$lang_home_preview = "Náhled";
+
+// Navigation  - Navigace
+$lang_nav_title = "Navigace";
+$lang_nav_home = "Domů";
+$lang_nav_blocks = "Blok";
+$lang_nav_img = "Média";
+$lang_nav_blog = "Blog";
+$lang_nav_pages = "Stránky";
+$lang_nav_stats = "Statistiky";
+
+// Navigation logged in bar  - Navigace v liště
+$lang_nav_logged_in_add_block = 'Přidat nový blok';
+$lang_nav_logged_in_add_blog_post = 'Přidat nový příspěvek blogu';
+$lang_nav_logged_in_add_media = 'Přidat nová média';
+$lang_nav_logged_in_add_page = 'Přidat novou stránku';
+
+// Gallery - Galerie
+$lang_gal_alt_gallery = "alt tag (popis)";
+$lang_gal_caption_gallery = "Zde vložte titulek";
+$lang_gal_filename = "Název souboru";
+$lang_gal_dimensions = "Rozměry";
+$lang_gal_size = "Velikost";
+$lang_gal_img = "IMG";
+$lang_gal_link = "Odkaz";
+$lang_gallery_tool_tip = "Chcete-li vytvořit lightbox galerii, vytvořte složku obrázků a použijte odkaz vložení.";
+$lang_crop = 'Oříznout';
+$lang_media_order = 'Pořadí';
+$lang_media_replace = 'Nahradit';
+
+// Pages  - Stránky
+$lang_pages_meta = "Meta";    /* Hlavička stránky */
+$lang_pages_title = "Název stránky";
+$lang_pages_description = "Popis stránky";
+$lang_pages_css = 'Stránky CSS';
+$lang_pages_js = 'Stránky JS';
+$lang_pages_template = 'Šablona stránky';
+
+// Create - Vytvořit
+$lang_create_file_or_folder = 'Nová stránka';
+$lang_create_file_or_folder_block = "Nový Blok";
+$lang_create_file_or_folder_blog  = "Nový Blog";
+$lang_create_file_or_folder_page  = "Nová Stránka";
+$lang_create_file_or_folder_media = "Nová Média";
+$lang_create_item_block = 'Vytvořit Blok';
+$lang_create_item_blog  = 'Vytvořit Blog';
+$lang_create_item_page  = 'Vytvoření stránky';
+$lang_create_item_media = 'Vytvořte položku Média';
+$lang_create_button = "Vytvořit"; 
+$lang_create_tool_tip = "Stránky, bloky a příspěvky na blogu musí končit příponou '.txt'. Chcete-li vytvořit složku, neuvadejte příponou '.txt'.";
+$lang_create_embed = "Odkaz";
+$lang_create_embed_tag = "Odkaz vložení.";
+$lang_create_embed_url = "Odkaz URL:";
+$lang_create_embed_html = "Odkaz HTML:";
+$lang_create_embed_php = "Odkaz PHP:";
+$lang_create_block_move = "Přesuňte blok do";
+
+// Delete - Vymazat
+$lang_delete = 'Jste si jisti, že to chcete smazat ';
+$lang_del_button = "Vymazat";
+
+// Errors - Chyby
+$lang_error_create_ext = 'Neplatná přípona souboru.';
+$lang_error_file_exists = 'Soubor s tímto názvem již v této složce existuje.';
+$lang_error_upload = 'Chcete-li nahrát obrázek, musíte být v galerii.';
+$lang_no_content = 'žádný obsah nebyl nalezen';
+
+//Blog - Blog
+$lang_blog_title = 'Název Blogu';
+$lang_blog_date = 'Datum';
+$lang_blog_read_more = 'Čtěte více';
+$lang_blog_back_button = 'Zpět';
+$lang_blog_older = 'Starší';
+$lang_blog_newer = 'Novější';
+$lang_blog_error_folder = "V Blogu nelze vytvořit složku";
+$lang_blog_tool_tip = "Položky blogu jsou řazeny podle čísla, nejvyšší číslování prozatím nejnovější příspěvek.";
+$lang_blog_import = 'Importovat';
+$lang_blog_import_tool_tip = 'Položky blogu importovat z RSS. Importér bude importovat z RSS a Atom formátů.';
+$lang_blog_import_page_max = 'Maximální počet stránek';
+$lang_blog_import_page_var = 'Název proměnné stránkováni';
+$lang_blog_import_url = 'URL';
+$lang_blog_description = 'Popis Blogu';
+$lang_blog_tags = 'Tagy blogu (oddělené čárkami)';
+$lang_blog_url = 'URL Blogu';
+$lang_blog_index = 'Chcete indexovat?';
+$lang_blog_nofollow = 'Přejete si "nesledovat?"';
+$lang_blog_custom = 'Vlastní popis META';
+
+//Form - Formulář
+$lang_form_error1 = 'Prosím vyplňte pole ';
+$lang_form_error2a = 'Vaše pole ';
+$lang_form_error2b = ' je příliš dlouhé.';
+$lang_form_subject_line = 'Kontakt';
+$lang_form_email_sent = 'E-mail byl odeslán';
+$lang_form_sent_button = 'Odeslat';
+$lang_auto_thank_contact = 'Děkuji za zaslání e-mailu! Na Váš e-mail odpovím jakmile to bude možné.';
+
+//Newsletter Form - Formulář zpravodaje
+$lang_newsletter_thanks ='Díky!';
+$lang_newsletter_try_again ='Zkuste to znovu';
+$lang_newsletter_placeholder ='Zadejte e-mail';
+$lang_newsletter_send ='Přihlásit se';
+
+// Stats - Statistiky
+$lang_stats_today = "Návštěvníci";
+$lang_stats_refers = "Top odkazovačů";
+$lang_stats_pages = "Top stránek";
+$lang_stats_refresh = "Aktualizovat";
+$lang_stats_pageviews = "Zobrazení stránek";
+$lang_stats_per_visit = "Stránky / Návštěva";
+$lang_stats_bounce_rate ="Míra hodnocení";
+$lang_stats_online = "Přítomen";
+$lang_stats_thisweek = "Tento týden";
+$lang_stats_todays_stats = "Dnes";
+$lang_stats_nodata = "Žádná data";
+$lang_stats_browsers  = "prohlížeče";
+$lang_stats_countries = "země";
+$lang_stats_devices   = "zařízení";
+$lang_stats_systems   = "Systémy";
+
+// Sweet Alert
+$lang_sweetalert_ok    = "OK";
+$lang_sweetalert_saved = "Uloženo";
+
+// Custom post types - Vlastní příspěvek
+$lang_custom_post_type_error_illegal_characters = 'Název příspěvku má nepovolené znaky';
+$lang_custom_post_type_error_illegal_name = 'Název příspěvku má nepovolen název';
+$lang_custom_post_type_error_unknown_entry = 'Tento příspěvek není znám';
+
+// Status messages - Stavové zprávy
+$lang_status_ok = 'Uložená data';
+
+// Dashboard - Navigační lišta
+$lang_dashboard_backups = 'Zálohování';
+$lang_dashboard_blocks = 'Bloky';
+$lang_dashboard_edit_blocks = 'Upravit Bloky';
+$lang_dashboard_blog = 'Blog';
+$lang_dashboard_edit_blog = 'Upravit Blog';
+$lang_dashboard_notes = 'Panel poznámek';
+$lang_dashboard_file_storage = 'Uložení souborů';
+$lang_dashboard_media = 'Media';
+$lang_dashboard_edit_media = 'Upravit Media';
+$lang_dashboard_pages = 'Stránky';
+$lang_dashboard_edit_page = 'Upravit stránku';
+
+// Settings
+$lang_settings_title = 'Pulse Nastaveí';
+$lang_settings_general = 'Všeobecné';
+$lang_settings_forms = 'Formuláře';
+$lang_settings_security = 'Bezpečnostní';
+$lang_settings_permissions = 'Oprávnění';
+$lang_settings_extend = 'Rozšíření';
+
+// Settings: General
+$lang_settings_path = 'Cesta';
+$lang_settings_path_tooltip = 'Ponechte prázdné, pokud je nainstalován v kořenové složce';
+$lang_settings_admin = 'Správce';
+$lang_settings_admin_tooltip = 'Název složky správce';
+$lang_settings_password = 'Heslo';
+$lang_settings_password_tooltip = 'Něco složitého uhodnout';
+$lang_settings_backup_tooltip = 'Automatické zálohování stránek';
+$lang_settings_backupemail = 'Zálohovací e-mail';
+$lang_settings_backupemail_tooltip = 'Místo, kam poslat záložní ZIP';
+$lang_settings_time = 'Výchozí časové pásmo';
+$lang_settings_language = 'Jazyk';
+$lang_settings_ip = 'Anonymizovat IP';
+$lang_settings_frontpage = 'Zobrazit titulní stránku';
+$lang_settings_adminfrontpage = 'Zobrazit titulní stránku správce';
+$lang_settings_adminname = 'Uživatelské jméno správce';
+$lang_settings_adminname_tooltip = 'Používá se k zobrazení názvu pro příspěvky v blogu';
+$lang_settings_css = 'CSS přepsání pro celý web';
+$lang_settings_css_tooltip = 'Vložte svůj CSS zde';
+$lang_settings_js = 'Přepis JS pro celé stránky';
+$lang_settings_js_tooltip = 'Vložte JS sem';
+
+// Settings: Cache
+$lang_settings_title_cache = 'Mezipamět';
+$lang_settings_cache = 'Povolit mezipaměť frontendu';
+
+// Settings: Editor
+$lang_settings_title_editor = 'Editor';
+$lang_settings_wysiwyg = 'Povolit WYSIWYG editor na blocích (jinak HTML a Markdown)';
+$lang_settings_upload = 'Povolit soubory nahrát';
+$lang_settings_upload_tooltip = 'Použijte čárku pro oddělené přípony souborů';
+$lang_settings_wysiwygpages = 'Povolit editor WYSIWYG na stránkách (jinak HTML a Markdown)';
+
+// Settings: Media
+$lang_settings_title_media = 'Media';
+$lang_settings_jpeg = 'Kvalita JPEG';
+$lang_settings_jpeg_tooltip = 'Použijte 100 pro plnou kvalitu JPEG (větší soubory)';
+$lang_settings_jpegresample = 'Převzorkování JPEG "vypnuto"';
+$lang_settings_jpegsize = 'Kvalita JPEG';
+$lang_settings_jpegsize_tooltip = 'Měřítko JPEG na maximální velikost (výška) pixelů';
+$lang_settings_thumbheight = 'Výška miniatury';
+$lang_settings_thumbheight_tooltip = 'Výška v px';
+$lang_settings_thumb = 'Vytvoření miniatury';
+
+// Settings: Navigation
+$lang_settings_title_navigation = 'Navigace';
+$lang_settings_navigationmenu = 'Povolit úpravu frontend';
+$lang_settings_title_managenavigation = 'Správa navigace na webu';
+
+// Settings: Google Analytics
+$lang_settings_title_google = 'Google analýza';
+$lang_settings_google = 'Kód sledování Analytics';
+$lang_settings_google_tooltip = 'Zde vložte úryvek kódu sledování Google Analytics';
+
+// Settings: OGP - Open Graph protocol >> http://ogp.me/
+$lang_settings_title_ogp = '(OGP) Otevřené protokoly grafika';
+$lang_settings_ogp = 'OGP výchozí obrázek';
+$lang_settings_ogp_tooltip = 'Relativní nebo absolutní odkaz';
+
+// Settings: Blog
+$lang_settings_title_blog = 'Blog';
+$lang_settings_blogresults = 'Výsledků na stránku';
+$lang_settings_blogresults_tooltip = 'Počet příspěvků blogu na stránku';
+$lang_settings_blogdisqus = 'Aktivovat Disqus komentáře';
+$lang_settings_blogdisqususer = 'Disqus uživatelské jméno';
+$lang_settings_blogdisqususer_tooltip = 'Přidejte krátké jméno účtu Disqus';
+$lang_settings_blogdate = 'Formát dátumu';
+$lang_settings_blogdate_tooltip = 'Formát dátumu v PHP. Více viz: https://php.net/manual/en/function.date.php';
+$lang_settings_blogmasonry = 'Povolit rozvržení blogu masonry';
+
+// Settings: RSS
+$lang_settings_title_rss = 'RSS';
+$lang_settings_rsstitle = 'Titul RSS blogu';
+$lang_settings_rsstitle_tooltip = 'Můj blog';
+$lang_settings_rssdescription = 'Popis blogu RSS';
+$lang_settings_rssdescription_tooltip = 'To je můj blog.';
+$lang_settings_rssurl = 'Adresa URL blogu';
+$lang_settings_rssurl_tooltip = 'http://example.com/blog';
+$lang_settings_rsslang = 'Kód jazyka'; // Formát dátumu
+$lang_settings_rsslang_tooltip = 'cs (http://www.rssboard.org/rss-language-codes)';
+$lang_settings_rssurlprefix = 'Předpona adres URL blogu';
+$lang_settings_rssurlprefix_tooltip = '(blog) byl by blog-1-název-příspěvku, také editovat v htaccess';
+
+// Settings: SMTP Settings
+$lang_settings_title_smtp = 'Nastavení SMTP';
+$lang_settings_smtpusername = 'SMTP uživatelské jméno';
+$lang_settings_smtpusername_tooltip = 'Použijte pouze, chcete-li použít protokol SMTP';
+$lang_settings_smtppassword = 'SMTP heslo';
+$lang_settings_smtppassword_tooltip = 'Heslo';
+$lang_settings_smtphost = 'Hostitel SMTP';
+$lang_settings_smtphost_tooltip = 'mail.server.com';
+$lang_settings_smtpport = 'SMTP port';
+$lang_settings_smtpport_tooltip = '465';
+
+// Settings: Pagination
+$lang_settings_title_pagination = 'Stránkování';
+$lang_settings_paginationlinks = 'odkazy na stránce zobrazovat';
+$lang_settings_paginationlinks_tooltip = 'Zobrazí se například na stránce Bloky a Stránky';
+$lang_settings_paginationpages = 'Výsledky na stránku';
+$lang_settings_paginationpages_tooltip = 'Kolik stránek se zobrazí na odkaz';
+
+// Settings: Forms
+$lang_settings_forminputname = 'Zadání názvu';
+$lang_settings_forminputname_tooltip = 'text';
+$lang_settings_forminputemail = 'E-mailový vstup';
+$lang_settings_forminputemail_tooltip = 'e-mail';
+$lang_settings_forminputtel = 'Tel vstup';
+$lang_settings_forminputtel_tooltip = 'text';
+$lang_settings_formnamename = 'Text pro název';
+$lang_settings_formnamename_tooltip = 'Název';
+$lang_settings_formemailname = 'Text pro e-mail';
+$lang_settings_formemailname_tooltip = 'E-mail';
+$lang_settings_formtextarea = 'Textové pole, velikost';
+$lang_settings_formtextarea_tooltip = 'Počet řádků v textovém poli komentářů';
+$lang_settings_formemail = 'Příjemce formuláře';
+$lang_settings_formemail_tooltip = 'Oddělit více příjemců čárkou';
+$lang_settings_formsubject = 'Formulář řádek předmětu';
+$lang_settings_formsubject_tooltip = 'Dostali jste e-mail';
+$lang_settings_formthanks = 'Po odeslání povolte automatickou odpověď díky';
+$lang_settings_formredirect = 'Přesměrování poděkování';
+$lang_settings_formredirect_tooltip = 'http://muj_web.cs/diky';
+
+// Settings: OTP One-Time Password
+$lang_settings_title_otp = 'Nastavení jednorázového hesla (Správce)';
+$lang_settings_otpactivate = 'Aktivujte jednoúčelové heslo';
+$lang_settings_otpsecret = 'Jednorázové tajné heslo';
+$lang_settings_otpsecret_tooltip = 'Získejte z mobilní aplikace OTP, například Google Authenticator';
+
+// Settings: reCAPTCHA
+$lang_settings_title_recap = 'reCAPTCHA';
+$lang_settings_recapkey = 'reCAPTCHA webový klíč (Site Key)';
+$lang_settings_recapkey_tooltip = 'Získejte z účtu Google: https://www.google.com/recaptcha';
+$lang_settings_recapkeysecret = 'reCAPTCHA Tajný klíč';
+$lang_settings_recapkeysecret_tooltip = 'Získejte z účtu Google: https://www.google.com/recaptcha';
+
+// Settings: Permissions - Oprávnění
+$lang_settings_title_editor = 'Uživatel Editoru';
+$lang_settings_editor = 'Povolit uživatele editoru';
+$lang_settings_editorpass = 'Heslo editoru';
+$lang_settings_editorpass_tooltip = 'Něco obtížného odhadnout';
+$lang_settings_editoruser = 'Uživatelské jméno editoru';
+$lang_settings_editoruser_tooltip = 'Používá se k zobrazení názvu pro příspěvky v blogu';
+$lang_settings_title_editorotp = 'Nastavení jednorázového hesla (Editora)';
+$lang_settings_editorotpactivate = 'Aktivujte jednoúčelové heslo';
+$lang_settings_editorotpsecret = 'Jednorázové tajné heslo';
+$lang_settings_editorotpsecret_tooltip = 'Získejte z mobilní aplikace OTP, například Google Authenticator';
+$lang_settings_title_editoraccess = 'Povolit Editor pro přístup k libovolným (nebo všechny) z těchto bloků:';
+$lang_settings_title_editorpagesaccess = 'Povolit Editor pro přístup k libovolným (nebo všechny) z těchto stránek:';
+
+// Settings: Extend
+$lang_settings_title_themes = 'Motivy (motivy stránek)';
+$lang_settings_themeselect = 'Motiv vybrán';
+$lang_settings_themeupload = 'Nahrát motiv ZIP';
+$lang_settings_title_plugins = 'Upravit vlastní položku příspěvku v:';
+$lang_settings_pluginupload = 'Nahrát plugin ZIP';
+$lang_settings_title_customposts = 'Vlastní typy příspěvků';
+$lang_settings_managecustomposts = 'Spravujte vlastní příspěvek';
+$lang_settings_themeuploader = 'Nahrát nový soubor ZIP s motivy';
+$lang_settings_pluginupload = 'Nahrajte nový soubor ZIP plugin';
+
+// Settings: Media
+$lang_settings_mediaupload = 'Nahrát mediální soubor';
+
+// Settings: Navigation
+$lang_settings_navigationtitle = 'Spravovat navigační odkazy';
+$lang_settings_navigationsubtitle = 'Přetáhněte a přesunte, chcete-li změnit pořadí seznamu.';
+$lang_settings_navigationsubmenu = 'Přidat podnabídku';
+
+// Settings: Custom Post Type
+$lang_settings_custompostentries = 'Vlastní položky příspěvku pro:';
+$lang_settings_custompostadd = 'Přidat novou položku';
+$lang_settings_title_custompostedit = 'Vlastní položky příspěvku pro:';
+$lang_settings_custompostback = '< Zpět na stránku seznamu';
+$lang_settings_custompostname = 'Název příspěvku';
+$lang_settings_custompostdelete = 'odstranit vlastní příspěvek';
+$lang_settings_custompostaddentry = 'Přidejte vlastní položku příspěvku pro:';
+$lang_settings_customposttypes = 'Vlastní typy příspěvků';
+$lang_settings_custompostmanage = 'spravovat záznamy';
+$lang_settings_custompostmanageedit = 'editovat';
+$lang_settings_custompostmanageadd = 'Přidat nový vlastní typ příspěvku';
+$lang_settings_title_custompostedit_title = 'Upravte vlastní typ příspěvku';
+$lang_settings_custompostdefadd = 'Přidejte vlastní typ příspěvku';
+$lang_settings_custompostdeftypename = 'Název typu příspěvku';
+$lang_settings_custompostdefname = 'Název';
+$lang_settings_custompostdeftype = 'Typ';
+$lang_settings_custompostdeffield = 'Pole';
+$lang_settings_custompostcontent = 'Obsah';
+
+// Search
+$lang_search_title = 'Vyhledávání';
+$lang_search_placeholder = 'Hledaný výraz';
+$lang_search_cta = 'Hledání';
+$lang_search_noresults = 'Nebyly nalezeny žádné výsledky';
+$lang_search_results = 'Výsledky vyhledávání';
+$lang_search_links = 'Odkaz na';
+
+// Page List
+$lang_pagelist = 'Seznam stránek';
+ ?>
