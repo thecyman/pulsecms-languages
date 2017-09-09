31674ee8d72f6a8ff043be071c0e34baca8b0db9
IvaRo <ivarosoft@gmail.com>
2017-09-09 07:31:30 +0200

    hungarian lang

---
+++ pulse 5/complete/hungarian.php
@@ -0,0 +1,404 @@
+<?php
+/*
+  * PulseCMS 5 nyelvek
+  * Fordítás: Magyar
+  * Szerző: IvaRo
+  * Kapcsolat: ivarosoft@gmail.com
+  * Copyright (c) 2017 IvaRo Soft Slovakia
+  * Engedély: Licence MIT
+*/
+
+// Global - Globális
+$lang_cancel = "Mégse";
+$lang_duplicate = "Duplikálás";
+$lang_help = "Súgó";
+$lang_help_url = "http://help.pulsecms.com/";
+$lang_settings = "Beállítások";
+$lang_account = "Fiók";
+$lang_save = "Mentés";
+$lang_rename_btn = "Átnevezés";
+$lang_title = "Pulse CMS";
+
+// Auto backup -  Auto mentés
+$lang_autobackup_email_subject_line = 'Pulse auto mentés emlékeztető';
+$lang_autobackup_email_text =<<<EOD
+Szia,
+
+A Pulse telepíteni generált biztonsági zip fájlt.
+
+Üdvözlettel,
+A Pulse CMS csapat
+EOD;
+
+// Login - Bejelentkezés
+$lang_login_forgot_password = "Elfelejtette a jelszavát?";
+$lang_login_forgot_password_email_body =<<<EOD
+Szia,
+
+Az Pulse jelszava PASSWORD
+EOD;
+$lang_login_forgot_password_email_subject_line = "Elfelejtett Pulse jelszavát?";
+$lang_login_incorrect = "A jelszó helytelen!";
+$lang_login_password = "Jelszó";
+$lang_login_button = "Bejelentkezés";
+$lang_nav_logout = "Kilépés";
+$lang_login_otp = "OTP jelszó";
+$lang_login_welcome = "Szia!";
+$lang_login_name = "Pulse Felhasználó";
+
+// Home - Főoldal
+$lang_home_emptyfold = "Ez a mappa üres.";
+$lang_home_new = "Új";
+$lang_home_upload_button = "Feltöltés";
+$lang_home_preview = "Megtekintése";
+
+// Navigation -  Navigáció
+$lang_nav_title = "Navigáció";
+$lang_nav_home = "Főoldal";
+$lang_nav_blocks = "Blokkok";
+$lang_nav_img = "Média";
+$lang_nav_blog = "Blog";
+$lang_nav_pages = "Oldalak";
+$lang_nav_stats = "Statisztikák";
+
+// Navigation logged in bar - Navigáció sáv
+$lang_nav_logged_in_add_block = 'Új blokk';
+$lang_nav_logged_in_add_blog_post = 'Új blogbejegyzés';
+$lang_nav_logged_in_add_media = 'Új média';
+$lang_nav_logged_in_add_page = 'Új oldal';
+
+// Gallery - Képtár
+$lang_gal_alt_gallery = "alt tag (leírás)";
+$lang_gal_caption_gallery = "A képaláírás itt legyen";
+$lang_gal_filename = "Fájlnév";
+$lang_gal_dimensions = "Méretek";
+$lang_gal_size = "Nagyság";
+$lang_gal_img = "IMG";
+$lang_gal_link = "Link";
+$lang_gallery_tool_tip = "A lightbox galériájának létrehozásához hozzon létre egy képmappát, és használja a beágyazott címkét.";
+$lang_crop = 'Képkivágás';
+$lang_media_order = 'Sorrend';
+$lang_media_replace = 'Cserélje';
+
+// Pages - Oldalak
+$lang_pages_meta = "Meta";
+$lang_pages_title = "Oldal címe";
+$lang_pages_description = "Oldalleírás";
+$lang_pages_css = 'Oldalak CSS';
+$lang_pages_js = 'Oldalak JS';
+$lang_pages_template = 'Oldalsablon';
+
+// Create - Létrehozás
+$lang_create_file_or_folder = 'Új oldal';
+$lang_create_file_or_folder_block = "Új Blokk";
+$lang_create_file_or_folder_blog  = "Új Blog";
+$lang_create_file_or_folder_page  = "Új Oldal";
+$lang_create_file_or_folder_media = "Új Média";
+$lang_create_item_block = 'Blokk létrehozása';
+$lang_create_item_blog  = 'Blog létrehozása';
+$lang_create_item_page  = 'Oldal létrehozása';
+$lang_create_item_media = 'Média létrehozása';
+$lang_create_button = "Létrehozása";
+$lang_create_tool_tip = "Az oldalak, blokkok és blogbejegyzéseknek '.txt'-el kell végződniük. Ahhoz, hogy hozzon létre egy mappát, ne adjon hozzá '.txt'";
+$lang_create_embed = "Beágyaz";
+$lang_create_embed_tag = "Beágyazott címke";
+$lang_create_embed_url = "Az URL beágyazása:";
+$lang_create_embed_html = "HTML beágyazása:";
+$lang_create_embed_php = "PHP beágyazása:";
+$lang_create_block_move = "Blokkot áthelyezni a";
+
+// Delete - Törlés
+$lang_delete = 'Biztosan törölni akarja';
+$lang_del_button = "Törlés";
+
+// Errors - Hibák
+$lang_error_create_ext = 'Érvénytelen fájlkiterjesztés.';
+$lang_error_file_exists = 'Ebben a mappában van egy ilyen nevű fájl.';
+$lang_error_upload = 'Kép kell feltöltenie a galériában.';
+$lang_no_content = 'nincs tartalom megtalálva';
+
+// Blog - Blog
+$lang_blog_title = 'Blog cím';
+$lang_blog_date = 'Dátum';
+$lang_blog_read_more = 'További információ';
+$lang_blog_back_button = 'Vissza';
+$lang_blog_older = 'Régebbi';
+$lang_blog_newer = "Újabb";
+$lang_blog_error_folder = "Nem hozhat létre mappát a Blogban";
+$lang_blog_tool_tip = "A blog bejegyzéseket szám szerint rendezi, a legmagasabb szám a legfrissebb bejegyzés.";
+$lang_blog_import = 'Importálás';
+$lang_blog_import_tool_tip = 'Blogbejegyzések importálása az RSS-ből. Az importőr importálja az RSS és ATOM formátumokat. ';
+$lang_blog_import_page_max = 'Max oldalszám';
+$lang_blog_import_page_var = 'Paging változó neve';
+$lang_blog_import_url = 'URL';
+$lang_blog_description = 'Blog Leírás';
+$lang_blog_tags = 'Blogcímkék (vesszővel elválasztva)';
+$lang_blog_url = 'Blog URL';
+$lang_blog_index = 'Szeretné ezt indexelni?';
+$lang_blog_nofollow = 'Szeretne "nem követni?"';
+$lang_blog_custom = 'Egyéni META leírás';
+
+// Form - Forma
+$lang_form_error1 = 'Kérjük töltse ki a mezőt';
+$lang_form_error2a = 'A meződ';
+$lang_form_error2b = 'túl hosszú.';
+$lang_form_subject_line = 'Kapcsolat';
+$lang_form_email_sent = 'Az e-mail elküldése';
+$lang_form_sent_button = 'Küldés';
+$lang_auto_thank_contact = 'Köszönjük, hogy küldött nekem egy e-mailt - hamarosan válaszolunk.';
+
+// Newsletter Form - Hírlevél űrlap
+$lang_newsletter_thanks = "Köszönöm!";
+$lang_newsletter_try_again = 'Próbálja újra';
+$lang_newsletter_placeholder = 'E-mail írása';
+$lang_newsletter_send = "Regisztráció";
+
+// Stats - Statisztika
+$lang_stats_today = "Látogatók";
+$lang_stats_refers = "Legnépszerűbb hivatkozások";
+$lang_stats_pages = "Legnépszerűbb oldalak";
+$lang_stats_refresh = "Frissítés";
+$lang_stats_pageviews = "Oldalmegtekintések";
+$lang_stats_per_visit = "Oldalak / látogatás";
+$lang_stats_bounce_rate = "visszafordulási arány";
+$lang_stats_online = "Kapcsolatban";
+$lang_stats_thisweek = "Ezen a héten";
+$lang_stats_todays_stats = "Ma";
+$lang_stats_nodata = "Nincs adat";
+$lang_stats_browsers = "böngészők";
+$lang_stats_countries = "országok";
+$lang_stats_devices = "eszközök";
+$lang_stats_systems = "rendszerek";
+
+// Sweet Alert
+$lang_sweetalert_ok    = "OK";
+$lang_sweetalert_saved = "Mentett";
+
+// Custom post types - Egyedi hozzászólástípusok
+$lang_custom_post_type_error_illegal_characters = 'Az egyéni bejegyzés típusának neve érvénytelen karakterekkel rendelkezik';
+$lang_custom_post_type_error_illegal_name = 'Az egyéni bejegyzés típusának neve érvénytelen névvel rendelkezik';
+$lang_custom_post_type_error_unknown_entry = 'Ez az egyedi bejegyzéstípus bejegyzés ismeretlen';
+
+// Status messages - Állapotüzenetek
+$lang_status_ok = 'Mentett adatok';
+
+// Dashboard - Irányítópult
+$lang_dashboard_backups = "Biztonsági mentések";
+$lang_dashboard_blocks = 'Blokkok';
+$lang_dashboard_edit_blocks = 'Blokkok szerkesztése';
+$lang_dashboard_blog = 'Blog';
+$lang_dashboard_edit_blog = 'Blog szerkesztése';
+$lang_dashboard_notes = 'Irányítópult megjegyzések';
+$lang_dashboard_file_storage = 'Fájl tárolása';
+$lang_dashboard_media = 'Média';
+$lang_dashboard_edit_media = 'Média szerkesztése';
+$lang_dashboard_pages = 'Oldalak';
+$lang_dashboard_edit_page = 'Oldal szerkesztése';
+
+// Settings
+$lang_settings_title = 'Pulse beállítások';
+$lang_settings_general = 'Általános';
+$lang_settings_forms = 'Formák';
+$lang_settings_security = 'Biztonság';
+$lang_settings_permissions = 'Engedélyek';
+$lang_settings_extend = 'Kiterjesztés';
+
+// Settings: General
+$lang_settings_path = 'Path';
+$lang_settings_path_tooltip = 'Hagyja üresen, ha a gyökérmappába telepíti';
+$lang_settings_admin = 'Adminisztrátor';
+$lang_settings_admin_tooltip = 'Admin mappa neve';
+$lang_settings_password = 'Jelszó';
+$lang_settings_password_tooltip = 'Valami nehéz kitalálni';
+$lang_settings_backup_tooltip = 'Automatikus webhely biztonsági mentés';
+$lang_settings_backupemail = 'Biztonsági mentés e-mailben';
+$lang_settings_backupemail_tooltip = 'A ZIP-mentés elküldésének helye';
+$lang_settings_time = 'Alapértelmezett időzóna';
+$lang_settings_language = 'Nyelv';
+$lang_settings_ip = 'IP anonimizálása';
+$lang_settings_frontpage = 'Címlap megjelenítése';
+$lang_settings_adminfrontpage = 'Adminisztrációs oldalmegjelenítés';
+$lang_settings_adminname = 'Adminisztrátori felhasználónév';
+$lang_settings_adminname_tooltip = 'A blogbejegyzések nevének megjelenítéséhez használt';
+$lang_settings_css = 'CSS felülírja az egész webhelyet';
+$lang_settings_css_tooltip = 'Illessze be ide a CSS-et';
+$lang_settings_js = 'A JS felülírja az egész webhelyet';
+$lang_settings_js_tooltip = 'Illessze be itt a JS-t';
+
+// Settings: Cache - Gyorsítótár
+$lang_settings_title_cache = 'Gyorsítótár';
+$lang_settings_cache = 'Az elülső gyorsítótár engedélyezése';
+
+// Settings: Editor
+$lang_settings_title_editor = 'Szerkesztő';
+$lang_settings_wysiwyg = 'Engedélyezze a WYSIWYG szerkesztőt blokkokra (egyébként HTML és Markdown)';
+$lang_settings_upload = 'Engedélyezett fájlok feltöltéséhez';
+$lang_settings_upload_tooltip = 'Használjon vesszőt a fájlkiterjesztések szétválasztásához';
+$lang_settings_wysiwygpages = 'Engedélyezze a WYSIWYG szerkesztőt a lapokon (egyébként HTML és Markdown)';
+
+// Settings: Media - Média
+$lang_settings_title_media = 'Média';
+$lang_settings_jpeg = 'JPEG minőség';
+$lang_settings_jpeg_tooltip = 'Használja 100-at a teljes JPEG minőséghez (nagyobb fájlok)';
+$lang_settings_jpegresample = 'Újramintavételezési JPEG "ki"';
+$lang_settings_jpegsize = 'JPEG minőség';
+$lang_settings_jpegsize_tooltip = 'A JPEG-ek méretaránya egy maximális képpont mérethez (magasság)';
+$lang_settings_thumbheight = 'A miniatűrök magassága';
+$lang_settings_thumbheight_tooltip = 'Magasság a px-ban';
+$lang_settings_thumb = 'Készítsen miniatűrök';
+
+// Settings: Navigation - Navigáció
+$lang_settings_title_navigation = 'Navigáció';
+$lang_settings_navigationmenu = 'Engedélyezze a frontend szerkesztését';
+$lang_settings_title_managenavigation = 'Webhely-navigáció kezelése';
+
+// Settings: Google Analytics - Google Analitika
+$lang_settings_title_google = 'Google Analitika';
+$lang_settings_google = 'Analytics követési kód';
+$lang_settings_google_tooltip = 'Illessze be ide a Google Analytics követőkódrészletét';
+
+// Settings: OGP - Open Graph protocol >> http://ogp.me/
+$lang_settings_title_ogp = '(OGP) Nytot grafikus protokollok';
+$lang_settings_ogp = 'OGP eredeti kép';
+$lang_settings_ogp_tooltip = 'Relatív vagy abszolút kapcsolat';
+
+// Settings: Blog
+$lang_settings_title_blog = 'Blog';
+$lang_settings_blogresults = 'Eredmények oldalanként';
+$lang_settings_blogresults_tooltip = 'Blogbejegyzések oldalanként';
+$lang_settings_blogdisqus = 'Disqus megjegyzések engedélyezése';
+$lang_settings_blogdisqususer = 'Disqus felhasználónév';
+$lang_settings_blogdisqususer_tooltip = 'Adja hozzá a Disqus fiók rövidnevét';
+$lang_settings_blogdate = 'Dátum formátum';
+$lang_settings_blogdate_tooltip = 'PHP dátumformátum. További információk: https://php.net/manual/en/function.date.php';
+$lang_settings_blogmasonry = 'Engedélyezze a blog masonry elrendezését';
+
+// Settings: RSS
+$lang_settings_title_rss = 'RSS';
+$lang_settings_rsstitle = 'RSS blog címe';
+$lang_settings_rsstitle_tooltip = 'Blogom';
+$lang_settings_rssdescription = 'RSS blog leírása';
+$lang_settings_rssdescription_tooltip = 'Ez a blogom.';
+$lang_settings_rssurl = 'Blog URL';
+$lang_settings_rssurl_tooltip = 'http://example.com/blog';
+$lang_settings_rsslang = 'Nyelvi kód'; // Dátum formátum
+$lang_settings_rsslang_tooltip = 'hu (http://www.rssboard.org/rss-language-codes)';
+$lang_settings_rssurlprefix = 'Blog URL előtag';
+$lang_settings_rssurlprefix_tooltip = '(blog) lenne blog-1-főcím-e, szintén szerkeszthető a htaccess-ban';
+
+// Settings: SMTP Settings
+$lang_settings_title_smtp = 'SMTP beállítások';
+$lang_settings_smtpusername = 'SMTP felhasználónév';
+$lang_settings_smtpusername_tooltip = 'Csak töltse ki, ha SMTP-t szeretne használni';
+$lang_settings_smtppassword = 'SMTP jelszó';
+$lang_settings_smtppassword_tooltip = 'Jelszó';
+$lang_settings_smtphost = 'SMTP-fogadó';
+$lang_settings_smtphost_tooltip = 'mail.server.com';
+$lang_settings_smtpport = 'SMTP port';
+$lang_settings_smtpport_tooltip = '465';
+
+// Settings: Pagination
+$lang_settings_title_pagination = 'Lapszámozás';
+$lang_settings_paginationlinks = 'Megjelenítendő oldalak linkjei';
+$lang_settings_paginationlinks_tooltip = 'Például a blokkok és oldalak navigációján jelenik meg';
+$lang_settings_paginationpages = 'Eredmények oldalanként';
+$lang_settings_paginationpages_tooltip = 'Hány oldalt jeleníthet meg linkenként';
+
+// Settings: Forms
+$lang_settings_forminputname = 'Bemeneti nev';
+$lang_settings_forminputname_tooltip = 'text';
+$lang_settings_forminputemail = 'E-mail bemenet';
+$lang_settings_forminputemail_tooltip = 'e-mail';
+$lang_settings_forminputtel = 'Tel bemenet';
+$lang_settings_forminputtel_tooltip = 'text';
+$lang_settings_formnamename = 'Szöveg a névhez';
+$lang_settings_formnamename_tooltip = 'Név';
+$lang_settings_formemailname = 'Az e-mail szövege';
+$lang_settings_formemailname_tooltip = 'E-mail';
+$lang_settings_formtextarea = 'Szövegterület mérete';
+$lang_settings_formtextarea_tooltip = 'Sorok száma a megjegyzés szöveges mezőben';
+$lang_settings_formemail = 'E-mail címzett';
+$lang_settings_formemail_tooltip = 'Több címzettet vesszővel elválasztva';
+$lang_settings_formsubject = 'Űrlap tárgya';
+$lang_settings_formsubject_tooltip = 'E-mailt kapott';
+$lang_settings_formthanks = 'Engedélyezze az automatikus köszönetet a válasz elküldése után';
+$lang_settings_formredirect = 'Átirányítani a köszönetet';
+$lang_settings_formredirect_tooltip = 'http://sajatwebhely.hu/kosz';
+
+// Settings: OTP One-Time Password
+$lang_settings_title_otp = 'Egyszeri jelszó beállítása (Adminisztrátor)';
+$lang_settings_otpactivate = 'Az egyszeri jelszó aktiválása';
+$lang_settings_otpsecret = 'Egyszeri titkos jelszó (OTP)';
+$lang_settings_otpsecret_tooltip = 'Kapjon OTP mobilalkalmazást, például a Google Hitelesítő';
+
+// Settings: reCAPTCHA
+$lang_settings_title_recap = 'reCAPTCHA';
+$lang_settings_recapkey = 'reCAPTCHA helyszíni kulcs';
+$lang_settings_recapkey_tooltip = 'Letöltés a Google-fiókból: https://www.google.com/recaptcha';
+$lang_settings_recapkeysecret = 'reCAPTCHA Titkos kulcs';
+$lang_settings_recapkeysecret_tooltip = 'Letöltés a Google-fiókból: https://www.google.com/recaptcha';
+
+// Settings: Permissions - Engedélyek
+$lang_settings_title_editor = 'Szerkesztő felhasználó';
+$lang_settings_editor = 'Engedélyezze a szerkesztő felhasználóját';
+$lang_settings_editorpass = 'Szerkesztő jelszó';
+$lang_settings_editorpass_tooltip = 'Valami nehéz kitalálni';
+$lang_settings_editoruser = 'Szerkesztő felhasználónév';
+$lang_settings_editoruser_tooltip = 'A blogbejegyzések nevének megjelenítéséhez használt';
+$lang_settings_title_editorotp = 'Egyszeri jelszó beállítások (Szerkesztő)';
+$lang_settings_editorotpactivate = 'Az egyszeri jelszó aktiválása';
+$lang_settings_editorotpsecret = 'Egyszeri titkos jelszó (OTP)';
+$lang_settings_editorotpsecret_tooltip = 'Kapjon OTP mobilalkalmazást, például a Google Hitelesítőt';
+$lang_settings_title_editoraccess = 'Engedélyezze a szerkesztőnek, hogy hozzáférjen ezekhez a blokkokhoz (vagy mindegyikhez):';
+$lang_settings_title_editorpagesaccess = 'Engedélyezze a szerkesztőnek, hogy hozzáférjen ezekhez az oldalakhoz (vagy mindegyikhez):';
+
+// Settings: Extend
+$lang_settings_title_themes = 'Témák';
+$lang_settings_themeselect = 'Téma kiválasztva';
+$lang_settings_themeupload = 'Téma ZIP feltöltése';
+$lang_settings_title_plugins = 'Edit a custom post type Egyéni bejegyzéstípushoz tartozó bejegyzés szerkesztése:entry in:';
+$lang_settings_pluginupload = 'Feltöltés plugin ZIP';
+$lang_settings_title_customposts = 'Saját bejegyzések';
+$lang_settings_managecustomposts = 'Egyéni bejegyzések kezelése';
+$lang_settings_themeuploader = 'Tölts fel egy új témát ZIP fájl';
+$lang_settings_pluginupload = 'Új plugin ZIP fájl feltöltése';
+
+// Settings: Media
+$lang_settings_mediaupload = 'Médiafájl feltöltése';
+
+// Settings: Navigation
+$lang_settings_navigationtitle = 'A navigációs hivatkozások kezelése';
+$lang_settings_navigationsubtitle = 'A lista átrendezéséhez húzza és vidd.';
+$lang_settings_navigationsubmenu = 'Adja hozzá az almenüt';
+
+// Settings: Custom Post Type
+$lang_settings_custompostentries = 'Egyéni bejegyzéstípus bejegyzések:';
+$lang_settings_custompostadd = 'Új bejegyzés hozzáadása';
+$lang_settings_title_custompostedit = 'Egyéni hozzászólás szerkesztése:';
+$lang_settings_custompostback = '< Vissza a lista oldalra';
+$lang_settings_custompostname = 'A bejegyzés neve';
+$lang_settings_custompostdelete = 'törölje saját bejegyzését';
+$lang_settings_custompostaddentry = 'Adjon meg egyéni bejegyzést a következőkhöz:';
+$lang_settings_customposttypes = 'Egyéni bejegyzés típusok';
+$lang_settings_custompostmanage = 'kezelése bejegyzés';
+$lang_settings_custompostmanageedit = 'szerkesztés';
+$lang_settings_custompostmanageadd = 'Adjon hozzá egy új egyéni bejegyzést';
+$lang_settings_title_custompostedit_title = 'A hozzájárulások egyedi kiigazítása';
+$lang_settings_custompostdefadd = 'Adjon meg egyéni bejegyzési típust';
+$lang_settings_custompostdeftypename = 'Hozzászólás típus neve';
+$lang_settings_custompostdefname = 'Név';
+$lang_settings_custompostdeftype = 'Típus';
+$lang_settings_custompostdeffield = 'Terület';
+$lang_settings_custompostcontent = 'Tartalom';
+
+// Search
+$lang_search_title = 'Keresés';
+$lang_search_placeholder = 'Keresési kifejezés';
+$lang_search_cta = 'Keres';
+$lang_search_noresults = 'Nincs találat';
+$lang_search_results = 'Keresési eredmények';
+$lang_search_links = 'Link ehhez';
+
+// Page List
+$lang_pagelist = 'Webhelylista';
+?>
