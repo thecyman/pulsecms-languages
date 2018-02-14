98a38c53b17d01d3abacff19def14988d9acd06b
yuzoolcode <michaelfrankland@yuzool.com>
2018-02-14 13:21:32 +0900

    Romanian added

---
+++ pulse 5/shipping-with/romanian.php
@@ -0,0 +1,441 @@
+<?php
+// Lang "RO"
+$lang_cancel = "Anulare";
+$lang_duplicate = "Duplicare";
+$lang_help = "Ajutor";
+$lang_help_url ="https://help.pulsecms.com/";
+$lang_settings = "Setari";
+$lang_account = "Cont";
+$lang_save = "Salveaza";
+$lang_rename_btn = "Redenumeste";
+$lang_title = "Pulse CMS";
+
+// Auto backup
+$lang_autobackup_email_subject_line = "Notificare Pulse auto-backup";
+$lang_autobackup_email_text =<<<EOD
+Buna,
+
+Instalarea ta Pulse a generat un fisier ZIP de backup.
+
+Salutari,
+Echipa Pulse CMS
+EOD;
+
+// Login
+$lang_login_forgot_password = "Ti-ai uitat parola ?";
+$lang_login_forgot_password_email_body =<<<EOD
+	Buna,
+	
+	Parola ta Pulse este PASSWORD
+EOD;
+$lang_login_forgot_password_email_subject_line = "Ti-ai uitat parola Pulse?"; 
+$lang_login_incorrect = "Parola ta nu este corecta!";
+$lang_login_password = "Parola";
+$lang_login_button = "Intra in cont";
+$lang_nav_logout = "Iesire din cont";
+$lang_login_otp = "Parola OTP";
+$lang_login_welcome = "Buna!";
+$lang_login_name = "Utilizator Pulse";
+
+// Home
+$lang_home_emptyfold = "Acest fisier este gol"; 
+$lang_home_new = "Nou";
+$lang_home_upload_button = "Incarcare";
+$lang_home_preview = "Previzualizare";
+
+// Navigation
+$lang_nav_title = "Navigare";
+$lang_nav_home = "Panou principal";
+$lang_nav_blocks = "Blocuri";
+$lang_nav_img = "Media";
+$lang_nav_blog = "Blog";
+$lang_nav_pages = "Pagini";
+$lang_nav_stats = "Statistici";
+
+// Navigation logged in bar
+$lang_nav_logged_in_add_block     = 'Adauga bloc nou';
+$lang_nav_logged_in_add_blog_post = 'Adauga postare de blog noua';
+$lang_nav_logged_in_add_media     = 'Adauga media';
+$lang_nav_logged_in_add_page      = 'Adauga pagina noua';
+
+// Gallery
+$lang_gal_alt_gallery = "eticheta alternativa (alt tag)";
+$lang_gal_caption_gallery = "Introdu legatura aici";
+$lang_gal_filename = "Nume fisier";
+$lang_gal_dimensions = "Dimensiuni";
+$lang_gal_size = "Marime";
+$lang_gal_img = "IMG";
+$lang_gal_link = "Link";
+$lang_gallery_tool_tip = "Pentru a crea o galerie lightbox, creati un folder de imagini si utilzati eticheta embed.";
+$lang_crop = 'Taie';
+$lang_media_order = 'Ordoneaza';
+$lang_media_replace = 'Substituie';
+
+// Pages
+$lang_pages_meta = "Meta";
+$lang_pages_title = "Titlul paginii";
+$lang_pages_description = "Descrierea paginii";
+$lang_pages_css = 'Inline CSS';
+$lang_pages_js = 'Inline JS';
+$lang_pages_template = 'Sablon pagina(template)';
+
+// Create
+$lang_create_file_or_folder = 'Pagina noua';
+$lang_create_file_or_folder_block = "Bloc Nou";
+$lang_create_file_or_folder_blog  = "Blog Nou";
+$lang_create_file_or_folder_page  = "Pagina Noua";
+$lang_create_file_or_folder_media = "Media Nou";
+
+$lang_create_item_block = 'Obiect Bloc';
+$lang_create_item_blog  = 'Obiect Blog';
+$lang_create_item_page  = 'Page';
+$lang_create_item_media = 'Obiect Media';
+$lang_create_item_folder = 'Folder';
+$lang_create_button = "Creaza"; 
+$lang_create_tool_tip = "Paginile, blocurile si postarile de blog trebuie incheiate cu '.txt', ce va fi automat adaugat. Pentru a crea un folder sau un blog nou, selectati Folder sau Blog si Creati.";
+$lang_create_embed = "Embed";
+$lang_create_embed_tag = "Embed Tag";
+$lang_create_embed_url = "Embed URL:";
+$lang_create_embed_html = "Embed HTML:";
+$lang_create_embed_php = "Embed PHP:";
+$lang_create_block_move = "Muta blocul la";
+$lang_create_media_move = "Muta media la";
+$lang_create_folder_error        = 'Nu a fost posibila crearea unui folder nou';
+$lang_create_folder_error_exists = 'Folderul nou exista deja';
+$lang_create_folder_error_no_base   = 'Nici un folder sursa nu a fost gasit';
+$lang_create_folder_error_no_folder = 'Nici un nume nu a fost introdus pentru folder';
+
+// Delete
+$lang_delete = 'Sunteti sigur ca doriti sa stergeti';
+$lang_del_button = "Sterge";
+
+// Errors
+$lang_error_create_ext = 'Extensia fisierului nu este valida.';
+$lang_error_file_exists = 'Un fisier cu acest nume exista deja in acest folder .';
+$lang_error_upload = 'Trebuie sa fi intr-o Galerie pentru a incarca o imagine.';
+$lang_no_content = 'niciun continut nu a fost gasit';
+
+//Blog
+$lang_blog_title = 'Titlul Blogului';
+$lang_blog_date = 'Data';
+$lang_blog_read_more = 'Citeste mai mult';
+$lang_blog_back_button = 'Inapoi';
+$lang_blog_older = 'Mai vechi';
+$lang_blog_newer = 'Mai noi';
+$lang_blog_error_folder = "Nu poti crea un folder in Blog";
+$lang_blog_tool_tip = "Intrarile de blog sunt ordonate dupa numar, cel mai mare numar reprezentand postarea cea mai noua.";
+$lang_blog_import = 'Import';
+$lang_blog_import_tool_tip = 'Importa intrari de blog din RSS. Importatorul va importa din formaturile RSS si ATOM.';
+$lang_blog_import_page_max = 'Numar maxi de pagini';
+$lang_blog_import_page_var = 'Numele variabilei de paginare';
+$lang_blog_import_url = 'URL';
+$lang_blog_description = 'Descrierea blogului';
+$lang_blog_tags = 'Etichetele Blogului (tags,separate prin virgula)';
+$lang_blog_url = 'URL-ul Blogului';
+$lang_blog_index = 'Doriti sa fie indexat?';
+$lang_blog_nofollow = 'Doriti ca acesta sa fie "nu urmariti?"';
+$lang_blog_custom = 'Descriere META personalizata';
+
+// Form
+$lang_form_error1 = 'Va rugam completati campul ';
+$lang_form_error2a = 'Campul dumneavoastra ';
+$lang_form_error2b = ' este prea lung.';
+$lang_form_subject_line = 'Contact';
+$lang_form_email_sent = 'Email-ul a fost trimis';
+$lang_form_sent_button = 'Trimite';
+$lang_auto_thank_contact = 'Iti multumesc pentru email - acesta a fost primit si vom raspunde in curand';
+
+//Newsletter Form
+$lang_newsletter_thanks ='Multumim!';
+$lang_newsletter_try_again ='Incearca din nou';
+$lang_newsletter_placeholder ='Introdu Email';
+$lang_newsletter_send ='Inscrie-te';
+
+// Redactor
+$lang_redactor_mail_button     = 'Adauga o adresa de Email';
+$lang_redactor_mail_link_title = 'Trimite email catre mine';
+
+// Stats
+$lang_stats_today = "Vizitatori";
+$lang_stats_refers = "Topul referentilor";
+$lang_stats_pages = "Topul paginilor";
+$lang_stats_refresh = "Refresh";
+$lang_stats_pageviews = "Vizualizari pagina";
+$lang_stats_per_visit = "Pagini / Vizita";
+$lang_stats_bounce_rate ="Rata de respingere";
+$lang_stats_online = "Online";
+$lang_stats_thisweek = "Aceasta saptamana";
+$lang_stats_todays_stats = "Azi";
+$lang_stats_nodata = "Nu exista date";
+$lang_stats_browsers  = "browsere";
+$lang_stats_countries = "tari";
+$lang_stats_devices   = "dispozitive";
+$lang_stats_systems   = "sisteme";
+
+// Sweet Alert
+$lang_sweetalert_ok    = "OK";
+$lang_sweetalert_saved = "Salvat";
+
+// Custom post types
+$lang_custom_post_type_error_illegal_characters = 'Numele personalizat pentru postare contine caractere ilegale';
+$lang_custom_post_type_error_illegal_name       = 'Numele personalizat pentru postare contine un nume ilegal';
+$lang_custom_post_type_error_unknown_entry      = 'Acest tip de intrare pentru postare este necunoscut';
+
+// Status messages
+$lang_status_ok = 'Datele au fost salvate';
+
+// Dashboard
+$lang_dashboard_backups = 'Backupuri';
+$lang_dashboard_blocks = 'Blocuri';
+$lang_dashboard_edit_blocks = 'Editeaza Blocuri';
+$lang_dashboard_blog = 'Blog';
+$lang_dashboard_edit_blog = 'Editeaza Blog';
+$lang_dashboard_notes = 'Notite Panou principal';
+$lang_dashboard_file_storage = 'Stocarea fisierelor';
+$lang_dashboard_media = 'Media';
+$lang_dashboard_edit_media = 'Editeaza Media';
+$lang_dashboard_pages = 'Pagini';
+$lang_dashboard_edit_page = 'Editeaza Pagina';
+
+// Settings
+$lang_settings_title = 'Setari Pulse';
+$lang_settings_general = 'General';
+$lang_settings_forms = 'Formulare';
+$lang_settings_security = 'Securitate';
+$lang_settings_permissions = 'Permisiuni';
+$lang_settings_extend = 'Extinde';
+
+//// Settings: General
+$lang_settings_path = 'Cale';
+$lang_settings_path_tooltip = 'Lasati necompletat daca este instalat in folderul radacina';
+$lang_settings_admin = 'Admin';
+$lang_settings_admin_tooltip = 'Nume folder pentru Admin';
+$lang_settings_password = 'Parola';
+$lang_settings_password_tooltip = 'Ceva greu de ghicit';
+$lang_settings_backup_tooltip = 'Backup automat al siteului';
+$lang_settings_backupemail = 'Backup pentru email';
+$lang_settings_backupemail_tooltip = 'Locul in care se trimite backupul ZIP';
+$lang_settings_time = 'Fus oras prestabilit(default)';
+$lang_settings_language = 'Limba';
+$lang_settings_ip = 'Anonimizare IP';
+$lang_settings_frontpage = 'Afisarea paginii principale';
+$lang_settings_adminfrontpage = 'Afisarea paginii principale de Admin';
+$lang_settings_adminname = 'Nume utilizator Admin';
+$lang_settings_adminname_tooltip = 'Folosit pentru a afisa numele pentru postari Blog';
+$lang_settings_css = 'suprascriere CSS pentru tot siteul';
+$lang_settings_css_tooltip = 'Copiati CSS aici';
+$lang_settings_js = 'suprascriere JS pentru tot siteul';
+$lang_settings_js_tooltip = 'Copiati JS aici';
+
+//// Settings: Cache
+$lang_settings_title_cache = 'Cache';
+$lang_settings_cache = 'Activati cache-ul frontend';
+
+//// Settings: Editor
+$lang_settings_title_editor = 'Editor';
+$lang_settings_wysiwyg = 'Permite editorul WYSIWYG pe Blocuri (altfel HTML si Markdown)';
+$lang_settings_upload = 'Fisierele permise pentru incarcare(upload)';
+$lang_settings_upload_tooltip = 'Foloseste virgula pentru a separa extensiile pentru fisier';
+$lang_settings_wysiwygpages = 'Permite editorul WYSIWYG pe Pagini (altfel HTML si Markdown)';
+
+//// Settings: Made in Pulse
+$lang_settings_title_made_in_pulse = 'Creat in Pulse';
+$lang_settings_made_in_pulse       = 'Insigna creat in pulse adaugata';
+$lang_settings_made_in_pulse_label = 'Permite continut creat-in-pulse pentru sablon';
+$lang_settings_theme_meta_author_show       = 'Arata meta autorului in tema';
+$lang_settings_theme_meta_author_show_label = 'Arata meta autorului in tema';
+
+//// Settings: Media
+$lang_settings_title_media = 'Media';
+$lang_settings_jpeg = 'Calitate JPEG';
+$lang_settings_jpeg_tooltip = 'Foloseste 100 pentru calitate maxima JPEG (fisiere mari)';
+$lang_settings_jpegresample = 'Resampling JPEG "oprit"';
+$lang_settings_jpegsize = 'calitate JPEG';
+$lang_settings_jpegsize_tooltip = 'Scaleaza JPEGs la o dimensiune maxima in pixeli (inaltime)';
+$lang_settings_thumbheight = 'Inaltime thumbnail';
+$lang_settings_thumbheight_tooltip = 'Inaltime in px';
+$lang_settings_thumb = 'Creati thumbnails';
+
+//// Settings: Navigation
+$lang_settings_title_navigation = 'Navigare';
+$lang_settings_navigationmenu = 'Permiteti editarea frontend';
+$lang_settings_title_managenavigation = 'Gestionati navigarea siteului';
+
+/// Settings: Google Analytics
+$lang_settings_title_google = 'Google Analytics';
+$lang_settings_google = 'Codul de urmarire Google Analytics';
+$lang_settings_google_tooltip = 'Copiati codul dvs. de urmarire/monitorizare Google Analytics aici';
+
+//// Settings: GeoIP
+$lang_settings_title_geoip = 'GeoIP';
+$lang_settings_geoip       = 'Date GeoIP pentru statistici despre site';
+$lang_settings_geoip_label = 'Activeaza GeoIP pentru statistici aici';
+
+//// Settings: OGP
+$lang_settings_title_ogp = 'OGP';
+$lang_settings_ogp = 'Imagine OGP implicita';
+$lang_settings_ogp_tooltip = 'Link relativ sau absolut';
+
+//// Settings: Blog
+$lang_settings_title_blog = 'Blog';
+$lang_settings_blogresults = 'Rezultate per pagina';
+$lang_settings_blogresults_tooltip = 'Postari de tip Blog per pagina';
+$lang_settings_blogdisqus = 'Permite comentarii Disqus';
+$lang_settings_blogdisqususer = 'Nume utilizator Disqus';
+$lang_settings_blogdisqususer_tooltip = 'Adaugati numele scurt aferent contului Disqus';
+$lang_settings_blogdate = 'Fomatul datei';
+$lang_settings_blogdate_tooltip = 'Format PHP al datei. Pentru mai multe detalii vedeti: https://php.net/manual/en/function.date.php';
+$lang_settings_blogmasonry = 'Activeaza schema pt Blog de tip zidarie(masonry layout)';
+
+//// Settings: RSS
+$lang_settings_title_rss = 'RSS';
+$lang_settings_rsstitle = 'Titlul blogului RSS ';
+$lang_settings_rsstitle_tooltip = 'Blogul meu';
+$lang_settings_rssdescription = 'Descrierea RSS a blogului';
+$lang_settings_rssdescription_tooltip = 'Acesta este blogul meu.';
+$lang_settings_rssurl = 'Blog URL';
+$lang_settings_rssurl_tooltip = 'http://example.com/blog';
+$lang_settings_rsslang = 'Formatul datei';
+$lang_settings_rsslang_tooltip = 'en-gb';
+$lang_settings_rssurlprefix = ' Prefix Blog URL';
+$lang_settings_rssurlprefix_tooltip = '(blog) ar fi blog-1-postare-titlu, de asemenea editati si in htaccess';
+
+//// Settings: SMTP Settings
+$lang_settings_title_smtp = 'Setari SMTP';
+$lang_settings_smtpusername = 'Nume utilizator SMTP';
+$lang_settings_smtpusername_tooltip = 'Completati doar daca doriti sa folositi SMTP';
+$lang_settings_smtppassword = 'SMTP parola';
+$lang_settings_smtppassword_tooltip = 'Parola';
+$lang_settings_smtphost = 'SMTP host';
+$lang_settings_smtphost_tooltip = 'mail.server.com';
+$lang_settings_smtpport = 'SMTP port';
+$lang_settings_smtpport_tooltip = '465';
+
+//// Settings: SweetAlert
+$lang_settings_title_sweetalert = 'Setari SweetAlert';
+$lang_settings_sweetalert_active_tooltip  = 'Activeaza alertele';
+$lang_settings_sweetalert_active          = 'Activeaza';
+$lang_settings_sweetalert_timeout_tooltip = 'Timeout delay (milliseconds)';
+$lang_settings_sweetalert_timeout         = 'Timeout';
+
+//// Settings: Pagination
+$lang_settings_title_pagination = 'Paginare';
+$lang_settings_paginationlinks = 'Linkuri de pagina pentru afisare';
+$lang_settings_paginationlinks_tooltip = 'Apare pe navigarea Blocurilor si Paginilor ca exemplu';
+$lang_settings_paginationpages = 'Rezultate per pagina';
+$lang_settings_paginationpages_tooltip = 'Cate pagini sa apara per link';
+
+//// Settings: Forms
+$lang_settings_forminputname = 'Introduceti numele';
+$lang_settings_forminputname_tooltip = 'text';
+$lang_settings_forminputemail = 'Introduceti emailul';
+$lang_settings_forminputemail_tooltip = 'email';
+$lang_settings_forminputtel = 'Introduceti telefonul';
+$lang_settings_forminputtel_tooltip = 'text';
+$lang_settings_formnamename = 'Textul pentru Nume';
+$lang_settings_formnamename_tooltip = 'Nume';
+$lang_settings_formemailname = 'Textul pentru Email';
+$lang_settings_formemailname_tooltip = 'Email';
+$lang_settings_formtextarea = 'Dimensiune textarea';
+$lang_settings_formtextarea_tooltip = 'Numarul de randuri in comentariul textarea';
+$lang_settings_formemail = 'Destinatarul formularului';
+$lang_settings_formemail_tooltip = 'Separati multiplii destinatari prin virgula';
+$lang_settings_formsubject = 'Linia de titlu a formularului';
+$lang_settings_formsubject_tooltip = 'Ati primit un email';
+$lang_settings_formthanks = 'Activeaza raspunsul automat de tip multumesc dupa inregistrare';
+$lang_settings_formredirect = 'Redirectionarea inregistrarii';
+$lang_settings_formredirect_tooltip = 'http://siteultau.com/multumiri';
+$lang_settings_formcomment             = 'Text/Denumire pentru comentariu';
+$lang_settings_formcommentname_tooltip = 'Comentariu';
+$lang_settings_formphone               = 'Text/Denumire pentru telefon';
+$lang_settings_formphonename_tooltip   = 'Telefon';
+
+//// Settings: OTP One-Time Password
+$lang_settings_title_otp = 'Setari OTP-parola dinamica (Admin)';
+$lang_settings_otpactivate = 'Activeaza OTP (parola dinamica)';
+$lang_settings_otpsecret = 'Secretul OTP (parola dinamica)';
+$lang_settings_otpsecret_tooltip = 'Obtine de la o aplicatie OTP pt mobil, cum ar fi Google Authenticator';
+
+//// Settings: reCAPTCHA
+$lang_settings_title_recap = 'reCAPTCHA';
+$lang_settings_recapkey = 'Cheia siteului reCAPTCHA ';
+$lang_settings_recapkey_tooltip = 'Obtine de la contul Google: https://www.google.com/recaptcha';
+$lang_settings_recapkeysecret = 'Cheia secreta reCAPTCHA ';
+$lang_settings_recapkeysecret_tooltip = 'Obtine de la contul Google: https://www.google.com/recaptcha';
+
+//// Settings: Permissions
+$lang_settings_title_editor = 'Utilizator al Editorului';
+$lang_settings_editor = 'Autorizeaza utilizatorul Editorului';
+$lang_settings_editorpass = 'Parola pentru Editor';
+$lang_settings_editorpass_tooltip = 'Ceva greu de ghicit';
+$lang_settings_editoruser = 'Nume utilizator pentru Editor';
+$lang_settings_editoruser_tooltip = 'Folosit pentru a afisa numele postarilor de tip Blog';
+$lang_settings_title_editorotp = 'Setari OTP-parola dinamica (Editor)';
+$lang_settings_editorotpactivate = 'Activeaza OTP (parola dinamica)';
+$lang_settings_editorotpsecret = 'Secretul OTP (parola dinamica)';
+$lang_settings_editorotpsecret_tooltip = 'Obtine de la o aplicatie OTP pt mobil, cum ar fi Google Authenticator';
+$lang_settings_title_editoraccess = 'Permite Editorului sa acceseze oricare dintre (sau toate) aceste blocuri:';
+$lang_settings_title_editorpagesaccess = 'Permite Editorului sa acceseze oricare dintre (sau toate) aceste pagini:';
+
+//// Settings: Extend
+$lang_settings_title_themes = 'Teme';
+$lang_settings_themeselect = 'Tema selectata';
+$lang_settings_themeupload = 'Incarca tema ZIP';
+$lang_settings_title_plugins = 'Plugins';
+$lang_settings_pluginupload = 'Incarca plugin ZIP';
+$lang_settings_title_customposts = 'Tipuri de postari personalizate';
+$lang_settings_managecustomposts = 'Gestioneaza tipurile de postari personalizate';
+$lang_settings_themeuploader = 'Incarcati o noua tema ca fisier ZIP';
+$lang_settings_pluginuploader = 'Incarcati un nou plugin ca fisier ZIP';
+
+//// Settings: Media
+$lang_settings_mediaupload = 'Incarca un fisier media';
+
+//// Settings: Navigation
+$lang_settings_navigationtitle = 'Gestioneaza linkurile de navigare';
+$lang_settings_navigationsubtitle = 'Drag and drop pentru a reordona lista.';
+$lang_settings_navigationsubmenu = 'Adauga sub-menu';
+$lang_settings_navigation_non_pulse_page = 'Adauga un obiect de tip non Pulse';
+$lang_settings_navigation_non_pulse_name = 'Nume';
+$lang_settings_navigation_non_pulse_url  = 'URL';
+$lang_settings_navigation_activate = 'Activeaza';
+
+//// Settings: Custom Post Type
+$lang_settings_custompostentries = 'Intrari de tip postare personalizata pentru:';
+$lang_settings_custompostadd = 'Adauga o noua intrare';
+$lang_settings_title_custompostedit = 'Intrari de tip postare personalizata pentru:';
+$lang_settings_custompostback = '< Inapoi la pagina listei';
+$lang_settings_custompostname = 'Numele Postarii';
+$lang_settings_custompostdelete = 'intrare de tip postare personalizata';
+$lang_settings_custompostaddentry = 'Adauga o intrare de tip postare personalizata pentru:';
+$lang_settings_customposttypes = 'Tipul postarii personalizate';
+$lang_settings_custompostmanage = 'gestioneaza intrarile';
+$lang_settings_custompostmanageedit = 'editeaza';
+$lang_settings_custompostmanageadd = 'Adauga un nou tip de postare personalizata';
+$lang_settings_title_custompostedit_title = 'Editati tipul unei postari personalizate';
+$lang_settings_title_custompostedit_titlein = 'Editati o intrare de tip postare personalizata in:';
+$lang_settings_custompostdefadd = 'Adaugati un tip de postare personalizata';
+$lang_settings_custompostdeftypename = 'Numele tipului de postare';
+$lang_settings_custompostdefname = 'Nume';
+$lang_settings_custompostdeftype = 'Tip';
+$lang_settings_custompostdeffield = 'Camp';
+$lang_settings_custompostcontent = 'Continut';
+
+// Tags
+$lang_settings_title_tags = 'Etichete';
+$lang_settings_manage_tags = 'Gestionati etichete';
+$lang_settings_tags_title = 'Gestionati etichetele';
+$lang_settings_tags_subtitle = 'Adauga / Sterge etichete si descrieri.';
+$lang_settings_tags_name = 'Nume';
+$lang_settings_tags_description = 'Descriere';
+
+// Search
+$lang_search_title = 'Cauta';
+$lang_search_placeholder = 'Termen de cautare';
+$lang_search_cta = 'Cauta';
+$lang_search_noresults = 'Niciun rezultat gasit';
+$lang_search_results = 'Rezultatele cautarii';
+$lang_search_links = 'Link catre';
+
+// Page List
+$lang_pagelist = 'Lista paginilor';
