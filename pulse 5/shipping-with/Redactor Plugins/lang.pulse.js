19da3c863c272948b3d14091e65a306ea7271a91
yuzoolcode <michaelfrankland@yuzool.com>
2018-03-16 13:08:28 +0900

    Redactor Plugins Translation for 5.1

---
+++ pulse 5/shipping-with/Redactor Plugins/lang.pulse.js
@@ -0,0 +1,668 @@
+/**
+ * translations for plugins
+ */
+(
+	function($R) {
+		
+		/**
+		 * english
+		 */
+		const EN = {
+			// alignment
+			"align": "Align",
+			"align-left": "Align Left",
+			"align-center": "Align Center",
+			"align-right": "Align Right",
+			"align-justify": "Align Justify",
+			
+			// clips
+			clips:"Clips",
+			"clips-select":"Please, select a clip",
+			
+			// file manager
+			choose: "Choose",
+			
+			// font color
+			fontcolor:"Text Color",
+			text:"Text",
+			highlight:"Highlight",
+			
+			// font family
+			fontfamily:"Font",
+			"remove-font-family":"Remove Font Family",
+			
+			// font size
+			size:"Size",
+			"remove-size":"Remove Font Size",
+			
+			// full screen
+			fullscreen:"Fullscreen",
+			
+			// image manager
+			choose:"Choose",
+			
+			// inline
+			style:"Style",
+			
+			// properties
+			properties:"Properties",
+			
+			// table
+			table:"Table",
+			"insert-table":"Insert table",
+			"insert-row-above":"Insert row above",
+			"insert-row-below":"Insert row below",
+			"insert-column-left":"Insert column left",
+			"insert-column-right":"Insert column right",
+			"add-head":"Add head",
+			"delete-head":"Delete head",
+			"delete-column":"Delete column",
+			"delete-row":"Delete row",
+			"delete-table":"Delete table",
+			
+			// text direction
+			"change-text-direction":"RTL-LTR",
+			"left-to-right":"Left to Right",
+			"right-to-left":"Right to Left",
+			
+			// video
+			video:"Video",
+			"video-html-code":"Video Embed Code or Youtube/Vimeo Link",
+			
+			// widget
+			widget:"Widget",
+			"widget-html-code":"Widget HTML Code"
+		};
+		
+		/**
+		 * japanese
+		 */
+		const JA = { /// ADD JAPANESE VARIATIONS HERE
+			// alignment
+			"align": "Align",
+			"align-left": "Align Left",
+			"align-center": "Align Center",
+			"align-right": "Align Right",
+			"align-justify": "Align Justify",
+			
+			// clips
+			clips:"Clips",
+			"clips-select":"Please, select a clip",
+			
+			// file manager
+			choose: "Choose",
+			
+			// font color
+			fontcolor:"Text Color",
+			text:"Text",
+			highlight:"Highlight",
+			
+			// font family
+			fontfamily:"Font",
+			"remove-font-family":"Remove Font Family",
+			
+			// font size
+			size:"Size",
+			"remove-size":"Remove Font Size",
+			
+			// full screen
+			fullscreen:"Fullscreen",
+			
+			// image manager
+			choose:"Choose",
+			
+			// inline
+			style:"Style",
+			
+			// properties
+			properties:"Properties",
+			
+			// table
+			table:"Table",
+			"insert-table":"Insert table",
+			"insert-row-above":"Insert row above",
+			"insert-row-below":"Insert row below",
+			"insert-column-left":"Insert column left",
+			"insert-column-right":"Insert column right",
+			"add-head":"Add head",
+			"delete-head":"Delete head",
+			"delete-column":"Delete column",
+			"delete-row":"Delete row",
+			"delete-table":"Delete table",
+			
+			// text direction
+			"change-text-direction":"RTL-LTR",
+			"left-to-right":"Left to Right",
+			"right-to-left":"Right to Left",
+			
+			// video
+			video:"Video",
+			"video-html-code":"Video Embed Code or Youtube/Vimeo Link",
+			
+			// widget
+			widget:"Widget",
+			"widget-html-code":"Widget HTML Code"
+		};
+		
+		/**
+		 * german
+		 */
+		const DE = { /// ADD GERMAN VARIATIONS HERE
+			// alignment
+			"align": "Align",
+			"align-left": "Align Left",
+			"align-center": "Align Center",
+			"align-right": "Align Right",
+			"align-justify": "Align Justify",
+			
+			// clips
+			clips:"Clips",
+			"clips-select":"Please, select a clip",
+			
+			// file manager
+			choose: "Choose",
+			
+			// font color
+			fontcolor:"Text Color",
+			text:"Text",
+			highlight:"Highlight",
+			
+			// font family
+			fontfamily:"Font",
+			"remove-font-family":"Remove Font Family",
+			
+			// font size
+			size:"Size",
+			"remove-size":"Remove Font Size",
+			
+			// full screen
+			fullscreen:"Fullscreen",
+			
+			// image manager
+			choose:"Choose",
+			
+			// inline
+			style:"Style",
+			
+			// properties
+			properties:"Properties",
+			
+			// table
+			table:"Table",
+			"insert-table":"Insert table",
+			"insert-row-above":"Insert row above",
+			"insert-row-below":"Insert row below",
+			"insert-column-left":"Insert column left",
+			"insert-column-right":"Insert column right",
+			"add-head":"Add head",
+			"delete-head":"Delete head",
+			"delete-column":"Delete column",
+			"delete-row":"Delete row",
+			"delete-table":"Delete table",
+			
+			// text direction
+			"change-text-direction":"RTL-LTR",
+			"left-to-right":"Left to Right",
+			"right-to-left":"Right to Left",
+			
+			// video
+			video:"Video",
+			"video-html-code":"Video Embed Code or Youtube/Vimeo Link",
+			
+			// widget
+			widget:"Widget",
+			"widget-html-code":"Widget HTML Code"
+		};
+		
+		/**
+		 * czech
+		 */
+		const CS = { /// ADD CZECH VARIATIONS HERE
+			// alignment
+			"align": "Align",
+			"align-left": "Align Left",
+			"align-center": "Align Center",
+			"align-right": "Align Right",
+			"align-justify": "Align Justify",
+			
+			// clips
+			clips:"Clips",
+			"clips-select":"Please, select a clip",
+			
+			// file manager
+			choose: "Choose",
+			
+			// font color
+			fontcolor:"Text Color",
+			text:"Text",
+			highlight:"Highlight",
+			
+			// font family
+			fontfamily:"Font",
+			"remove-font-family":"Remove Font Family",
+			
+			// font size
+			size:"Size",
+			"remove-size":"Remove Font Size",
+			
+			// full screen
+			fullscreen:"Fullscreen",
+			
+			// image manager
+			choose:"Choose",
+			
+			// inline
+			style:"Style",
+			
+			// properties
+			properties:"Properties",
+			
+			// table
+			table:"Table",
+			"insert-table":"Insert table",
+			"insert-row-above":"Insert row above",
+			"insert-row-below":"Insert row below",
+			"insert-column-left":"Insert column left",
+			"insert-column-right":"Insert column right",
+			"add-head":"Add head",
+			"delete-head":"Delete head",
+			"delete-column":"Delete column",
+			"delete-row":"Delete row",
+			"delete-table":"Delete table",
+			
+			// text direction
+			"change-text-direction":"RTL-LTR",
+			"left-to-right":"Left to Right",
+			"right-to-left":"Right to Left",
+			
+			// video
+			video:"Video",
+			"video-html-code":"Video Embed Code or Youtube/Vimeo Link",
+			
+			// widget
+			widget:"Widget",
+			"widget-html-code":"Widget HTML Code"
+		};
+		
+		/**
+		 * dutch
+		 */
+		const NL = { /// ADD DUTCH VARIATIONS HERE
+			// alignment
+			"align": "Align",
+			"align-left": "Align Left",
+			"align-center": "Align Center",
+			"align-right": "Align Right",
+			"align-justify": "Align Justify",
+			
+			// clips
+			clips:"Clips",
+			"clips-select":"Please, select a clip",
+			
+			// file manager
+			choose: "Choose",
+			
+			// font color
+			fontcolor:"Text Color",
+			text:"Text",
+			highlight:"Highlight",
+			
+			// font family
+			fontfamily:"Font",
+			"remove-font-family":"Remove Font Family",
+			
+			// font size
+			size:"Size",
+			"remove-size":"Remove Font Size",
+			
+			// full screen
+			fullscreen:"Fullscreen",
+			
+			// image manager
+			choose:"Choose",
+			
+			// inline
+			style:"Style",
+			
+			// properties
+			properties:"Properties",
+			
+			// table
+			table:"Table",
+			"insert-table":"Insert table",
+			"insert-row-above":"Insert row above",
+			"insert-row-below":"Insert row below",
+			"insert-column-left":"Insert column left",
+			"insert-column-right":"Insert column right",
+			"add-head":"Add head",
+			"delete-head":"Delete head",
+			"delete-column":"Delete column",
+			"delete-row":"Delete row",
+			"delete-table":"Delete table",
+			
+			// text direction
+			"change-text-direction":"RTL-LTR",
+			"left-to-right":"Left to Right",
+			"right-to-left":"Right to Left",
+			
+			// video
+			video:"Video",
+			"video-html-code":"Video Embed Code or Youtube/Vimeo Link",
+			
+			// widget
+			widget:"Widget",
+			"widget-html-code":"Widget HTML Code"
+		};
+		
+		/**
+		 * hungarian
+		 */
+		const HU = { /// ADD HUNGARIAN VARIATIONS HERE
+			// alignment
+			"align": "Align",
+			"align-left": "Align Left",
+			"align-center": "Align Center",
+			"align-right": "Align Right",
+			"align-justify": "Align Justify",
+			
+			// clips
+			clips:"Clips",
+			"clips-select":"Please, select a clip",
+			
+			// file manager
+			choose: "Choose",
+			
+			// font color
+			fontcolor:"Text Color",
+			text:"Text",
+			highlight:"Highlight",
+			
+			// font family
+			fontfamily:"Font",
+			"remove-font-family":"Remove Font Family",
+			
+			// font size
+			size:"Size",
+			"remove-size":"Remove Font Size",
+			
+			// full screen
+			fullscreen:"Fullscreen",
+			
+			// image manager
+			choose:"Choose",
+			
+			// inline
+			style:"Style",
+			
+			// properties
+			properties:"Properties",
+			
+			// table
+			table:"Table",
+			"insert-table":"Insert table",
+			"insert-row-above":"Insert row above",
+			"insert-row-below":"Insert row below",
+			"insert-column-left":"Insert column left",
+			"insert-column-right":"Insert column right",
+			"add-head":"Add head",
+			"delete-head":"Delete head",
+			"delete-column":"Delete column",
+			"delete-row":"Delete row",
+			"delete-table":"Delete table",
+			
+			// text direction
+			"change-text-direction":"RTL-LTR",
+			"left-to-right":"Left to Right",
+			"right-to-left":"Right to Left",
+			
+			// video
+			video:"Video",
+			"video-html-code":"Video Embed Code or Youtube/Vimeo Link",
+			
+			// widget
+			widget:"Widget",
+			"widget-html-code":"Widget HTML Code"
+		};
+		
+		/**
+		 * polish
+		 */
+		const PL = { /// ADD POLISH VARIATIONS HERE
+			// alignment
+			"align": "Align",
+			"align-left": "Align Left",
+			"align-center": "Align Center",
+			"align-right": "Align Right",
+			"align-justify": "Align Justify",
+			
+			// clips
+			clips:"Clips",
+			"clips-select":"Please, select a clip",
+			
+			// file manager
+			choose: "Choose",
+			
+			// font color
+			fontcolor:"Text Color",
+			text:"Text",
+			highlight:"Highlight",
+			
+			// font family
+			fontfamily:"Font",
+			"remove-font-family":"Remove Font Family",
+			
+			// font size
+			size:"Size",
+			"remove-size":"Remove Font Size",
+			
+			// full screen
+			fullscreen:"Fullscreen",
+			
+			// image manager
+			choose:"Choose",
+			
+			// inline
+			style:"Style",
+			
+			// properties
+			properties:"Properties",
+			
+			// table
+			table:"Table",
+			"insert-table":"Insert table",
+			"insert-row-above":"Insert row above",
+			"insert-row-below":"Insert row below",
+			"insert-column-left":"Insert column left",
+			"insert-column-right":"Insert column right",
+			"add-head":"Add head",
+			"delete-head":"Delete head",
+			"delete-column":"Delete column",
+			"delete-row":"Delete row",
+			"delete-table":"Delete table",
+			
+			// text direction
+			"change-text-direction":"RTL-LTR",
+			"left-to-right":"Left to Right",
+			"right-to-left":"Right to Left",
+			
+			// video
+			video:"Video",
+			"video-html-code":"Video Embed Code or Youtube/Vimeo Link",
+			
+			// widget
+			widget:"Widget",
+			"widget-html-code":"Widget HTML Code"
+		};
+		
+		/**
+		 * romanian
+		 */
+		const RO = { /// ADD ROMANIAN VARIATIONS HERE
+			// alignment
+			"align": "Align",
+			"align-left": "Align Left",
+			"align-center": "Align Center",
+			"align-right": "Align Right",
+			"align-justify": "Align Justify",
+			
+			// clips
+			clips:"Clips",
+			"clips-select":"Please, select a clip",
+			
+			// file manager
+			choose: "Choose",
+			
+			// font color
+			fontcolor:"Text Color",
+			text:"Text",
+			highlight:"Highlight",
+			
+			// font family
+			fontfamily:"Font",
+			"remove-font-family":"Remove Font Family",
+			
+			// font size
+			size:"Size",
+			"remove-size":"Remove Font Size",
+			
+			// full screen
+			fullscreen:"Fullscreen",
+			
+			// image manager
+			choose:"Choose",
+			
+			// inline
+			style:"Style",
+			
+			// properties
+			properties:"Properties",
+			
+			// table
+			table:"Table",
+			"insert-table":"Insert table",
+			"insert-row-above":"Insert row above",
+			"insert-row-below":"Insert row below",
+			"insert-column-left":"Insert column left",
+			"insert-column-right":"Insert column right",
+			"add-head":"Add head",
+			"delete-head":"Delete head",
+			"delete-column":"Delete column",
+			"delete-row":"Delete row",
+			"delete-table":"Delete table",
+			
+			// text direction
+			"change-text-direction":"RTL-LTR",
+			"left-to-right":"Left to Right",
+			"right-to-left":"Right to Left",
+			
+			// video
+			video:"Video",
+			"video-html-code":"Video Embed Code or Youtube/Vimeo Link",
+			
+			// widget
+			widget:"Widget",
+			"widget-html-code":"Widget HTML Code"
+		};
+		
+		/**
+		 * slovak
+		 */
+		const SK = { /// ADD SLOVAK VARIATIONS HERE
+			// alignment
+			"align": "Align",
+			"align-left": "Align Left",
+			"align-center": "Align Center",
+			"align-right": "Align Right",
+			"align-justify": "Align Justify",
+			
+			// clips
+			clips:"Clips",
+			"clips-select":"Please, select a clip",
+			
+			// file manager
+			choose: "Choose",
+			
+			// font color
+			fontcolor:"Text Color",
+			text:"Text",
+			highlight:"Highlight",
+			
+			// font family
+			fontfamily:"Font",
+			"remove-font-family":"Remove Font Family",
+			
+			// font size
+			size:"Size",
+			"remove-size":"Remove Font Size",
+			
+			// full screen
+			fullscreen:"Fullscreen",
+			
+			// image manager
+			choose:"Choose",
+			
+			// inline
+			style:"Style",
+			
+			// properties
+			properties:"Properties",
+			
+			// table
+			table:"Table",
+			"insert-table":"Insert table",
+			"insert-row-above":"Insert row above",
+			"insert-row-below":"Insert row below",
+			"insert-column-left":"Insert column left",
+			"insert-column-right":"Insert column right",
+			"add-head":"Add head",
+			"delete-head":"Delete head",
+			"delete-column":"Delete column",
+			"delete-row":"Delete row",
+			"delete-table":"Delete table",
+			
+			// text direction
+			"change-text-direction":"RTL-LTR",
+			"left-to-right":"Left to Right",
+			"right-to-left":"Right to Left",
+			
+			// video
+			video:"Video",
+			"video-html-code":"Video Embed Code or Youtube/Vimeo Link",
+			
+			// widget
+			widget:"Widget",
+			"widget-html-code":"Widget HTML Code"
+		};
+		
+		/**
+		 * add translations
+		 */
+		$R.addLang('ar',    EN);
+		$R.addLang('cs',    EN);
+		$R.addLang('da',    EN);
+		$R.addLang('de',    EN);
+		$R.addLang('en',    EN);
+		$R.addLang('es',    EN);
+		$R.addLang('fa',    EN);
+		$R.addLang('fi',    EN);
+		$R.addLang('fr',    EN);
+		$R.addLang('he',    EN);
+		$R.addLang('hu',    EN);
+		$R.addLang('it',    EN);
+		$R.addLang('ja',    JA);
+		$R.addLang('ko',    EN);
+		$R.addLang('nl',    EN);
+		$R.addLang('no',    EN);
+		$R.addLang('pl',    EN);
+		$R.addLang('pt_br', EN);
+		$R.addLang('ro',    EN);
+		$R.addLang('ru',    EN);
+		$R.addLang('sk',    EN);
+		$R.addLang('sl',    EN);
+		$R.addLang('sv',    EN);
+		$R.addLang('tr',    EN);
+		$R.addLang('zh_cn', EN);
+		$R.addLang('zh_tw', EN);
+		
+	}
+)(Redactor);
